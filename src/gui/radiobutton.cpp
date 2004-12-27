e5ba1c69c54cb7e7abbe2bab67aff46255575d0e
Mateusz Kaduk <mateusz.kaduk@gmail.com>
2004-12-27 23:09:09 +0000

    Added radiobutton class

---
+++ src/gui/radiobutton.cpp
@@ -0,0 +1,53 @@
+/*
+ *  The Mana World
+ *  Copyright 2004 The Mana World Development Team
+ *
+ *  This file is part of The Mana World.
+ *
+ *  The Mana World is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  The Mana World is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with The Mana World; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  $Id$
+ */
+
+#include "radiobutton.h"
+
+RadioButton::RadioButton(const std::string& caption, const std::string& group, bool marked):
+    gcn::RadioButton(caption,group, marked)
+{
+}
+
+void RadioButton::drawBox(gcn::Graphics* graphics) {
+    BITMAP *box = NULL;
+    int x, y;
+
+    getAbsolutePosition(x, y);
+
+    if (mMarked) {
+        if (false /*disabled*/) {
+            box = gui_skin.radiobutton.disabled_checked;
+        } else {
+            box = gui_skin.radiobutton.checked;
+        }
+    } else if (false /*disabled*/) {
+        box = gui_skin.radiobutton.disabled;
+    } else {
+        box = gui_skin.radiobutton.normal;
+    }
+
+    x += 2;
+    y += 2;
+    if(box != NULL)
+	masked_blit(box, gui_bitmap, 0, 0, x, y, box->w, box->h);
+}
