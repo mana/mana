c209e6fcc6075e91bacfe54b2fc637623afdbfca
Kiyoshi Kyokai <kiyoshi.kyokai@gmail.com>
2005-02-28 14:16:18 +0000

    this is the new dialog for charging attacks and such. To be implemented soon,

---
+++ src/gui/chargedialog.cpp
@@ -0,0 +1,51 @@
+/*
+ *  The Mana World
+ *  Copyright 2004 The Mana World Development Team
+ *
+ *  This file is part of The Mana World.
+ *
+ *  The Mana World is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  The Mana World is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with The Mana World; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+ /* The window supported by this class shows player stats and keeps a charging
+  * action bar in queue.
+  */
+
+#include "chargedialog.h"
+#include "listbox.h"
+#include "scrollarea.h"
+#include "button.h"
+#include "../main.h"
+
+
+ChargeDialog::ChargeDialog():
+    Window("")
+{
+    setSize(200,100);
+    progBar = new ProgressBar(0.0f,50,40,100,15,128,128,128);
+    add(progBar);
+}
+
+ChargeDialog::~ChargeDialog()
+{
+ 
+}
+
+void ChargeDialog::action(const std::string& eventId)
+{
+
+}
+
