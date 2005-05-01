361a7cb45ec817b5fc4c8d35070cad527ba9cc51
Bjørn Lindeijer <bjorn@lindeijer.nl>
2005-05-01 11:17:41 +0000

    Adding safety check to walk check.

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -103,7 +103,7 @@ void Gui::mousePress(int mx, int my, int button)
     // Mouse pressed on window container (basically, the map)
 
     // When conditions for walking are met, set new player destination
-    if (player_node->action != DEAD && current_npc == 0 &&
+    if (player_node && player_node->action != DEAD && current_npc == 0 &&
             button == gcn::MouseInput::LEFT)
     {
         int tilex = mx / 32 + camera_x;
