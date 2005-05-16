165b87d22f6710003868e32ed52645447be5d538
Mateusz Kaduk <mateusz.kaduk@gmail.com>
2005-05-16 23:25:36 +0000

    Added menu

---
+++ src/gui/menu.h
@@ -0,0 +1,42 @@
+/*
+ *  The Mana World
+ *  Copyright 2004 The Mana World Development Team
+ *
+ *  This file is part of The Mana World.
+ *
+ *  The Mana World is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  The Mana World is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with The Mana World; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  $Id$
+ */
+
+#ifndef _TMW_MENU_H
+#define _TMW_MENU_H
+
+#include <guichan.hpp>
+#include "menuitem.h"
+
+class Menu : public gcn::Widget
+{
+    public:
+        /**
+         * Constructor.
+         */
+        Menu(const std::vector<MenuItem>& items);
+
+    private:
+        int i;
+};
+
+#endif /* _TMW_MENU_H */
