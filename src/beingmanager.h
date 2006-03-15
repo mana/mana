0d7f2d306d2899571b6034ccd3de6e3a34c2193a
Bjørn Lindeijer <bjorn@lindeijer.nl>
2006-03-15 15:30:51 +0000

    Fix source header which was somehow completely in lowercase.

--- src/beingmanager.h
+++ src/beingmanager.h
@@ -1,24 +1,24 @@
 /*
- *  the mana world
- *  copyright 2004 the mana world development team
+ *  The Mana World
+ *  Copyright 2004 The Mana World Development Team
  *
- *  this file is part of the mana world.
+ *  This file is part of The Mana World.
  *
- *  the mana world is free software; you can redistribute it and/or modify
- *  it under the terms of the gnu general public license as published by
- *  the free software foundation; either version 2 of the license, or
+ *  The Mana World is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
  *  any later version.
  *
- *  the mana world is distributed in the hope that it will be useful,
- *  but without any warranty; without even the implied warranty of
- *  merchantability or fitness for a particular purpose.  see the
- *  gnu general public license for more details.
+ *  The Mana World is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *  you should have received a copy of the gnu general public license
- *  along with the mana world; if not, write to the free software
- *  foundation, inc., 59 temple place, suite 330, boston, ma  02111-1307  usa
+ *  You should have received a copy of the GNU General Public License
+ *  along with The Mana World; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *  $id: being.cpp,v 1.98 2005/12/25 01:28:03 b_lindeijer exp $
+ *  $Id$
  */
 
 #ifndef _TMW_BEINGMANAGER_H
