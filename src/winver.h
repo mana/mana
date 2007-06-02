7f5ecb3d24df397cdee57e545fae42a8e11024f8
Bjørn Lindeijer <bjorn@lindeijer.nl>
2007-06-02 09:25:18 +0000

    Updated version and release date and summarized changes.

--- src/winver.h
+++ src/winver.h
@@ -1,6 +1,6 @@
 /* VERSION DEFINITIONS */
 #define VER_MAJOR 0
 #define VER_MINOR 0
-#define VER_RELEASE 22
-#define VER_BUILD 2
-#define PACKAGE_VERSION "0.0.22.2"
+#define VER_RELEASE 23
+#define VER_BUILD 0
+#define PACKAGE_VERSION "0.0.23"
