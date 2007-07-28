94dd102a2d4103454632813603d9a503c9bda8cd
Philipp Sehmisch <tmw@crushnet.org>
2007-07-28 19:47:20 +0000

    forgot to commit a file with the last commit

---
+++ src/resources/equipmentinfo.cpp
@@ -0,0 +1,82 @@
+/*
+ *  The Mana World
+ *  Copyright 2006 The Mana World Development Team
+ *
+ *  This file is part of The Mana World.
+ *
+ *  The Mana World is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  The Mana World is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with The Mana World; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  $Id$
+ */
+
+#include "equipmentinfo.h"
+
+void
+EquipmentInfo::setAttackType(std::string attackType)
+{
+    if (attackType == "swing")
+    {
+        mAttackType = ACTION_ATTACK_SWING;
+    }
+    else if (attackType == "stab")
+    {
+        mAttackType = ACTION_ATTACK_STAB;
+    }
+    else if (attackType == "bow")
+    {
+        mAttackType = ACTION_ATTACK_BOW;
+    }
+    else if (attackType == "throw")
+    {
+        mAttackType = ACTION_ATTACK_THROW;
+    }
+    else if (attackType == "none")
+    {
+        mAttackType = ACTION_DEFAULT;
+    }
+    else
+    {
+        mAttackType = ACTION_ATTACK;
+    }
+}
+
+void
+EquipmentInfo::addSound (EquipmentSoundEvent event, std::string filename)
+{
+    if (mSounds.find(event) == mSounds.end())
+    {
+        mSounds[event] = new std::vector<std::string>;
+    }
+
+    mSounds[event]->push_back("sfx/" + filename);
+}
+
+
+std::string
+EquipmentInfo::getSound (EquipmentSoundEvent event) const
+{
+    std::map<EquipmentSoundEvent, std::vector<std::string>* >::const_iterator i;
+
+    i = mSounds.find(event);
+
+    if (i == mSounds.end())
+    {
+        return "";
+    }
+    else
+    {
+        return i->second->at(rand()%i->second->size());
+    }
+}
