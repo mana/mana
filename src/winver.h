700b390ee8cf1ab9468999d1c10188da65e5fb51
Bjørn Lindeijer <bjorn@lindeijer.nl>
2007-12-22 15:24:14 +0000

    Updated version to 0.0.24. Release data set to tomorrow, we'll see.

--- src/winver.h
+++ src/winver.h
@@ -1,6 +1,6 @@
 /* VERSION DEFINITIONS */
 #define VER_MAJOR 0
 #define VER_MINOR 0
-#define VER_RELEASE 23
+#define VER_RELEASE 24
 #define VER_BUILD 0
-#define PACKAGE_VERSION "0.0.23"
+#define PACKAGE_VERSION "0.0.24"
