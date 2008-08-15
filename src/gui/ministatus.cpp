4843a33a35cf46fe7978c59c0bf9b5b5e7c885c8
Lloyd Bryant <lloyd_bryant@netzero.net>
2008-08-15 04:05:00 +0000

    Activated MP status progress bar

--- src/gui/ministatus.cpp
+++ src/gui/ministatus.cpp
@@ -98,7 +98,7 @@ void MiniStatusWindow::update()
     }
 
     mHpBar->setProgress((float) player_node->mHp / player_node->mMaxHp);
-    // mMpBar->setProgress((float) player_node->mMp / player_node->mMaxMp);
+    mMpBar->setProgress((float) player_node->mMp / player_node->mMaxMp);
     mXpBar->setProgress(
             (float) player_node->getXp() / player_node->mXpForNextLevel);
 
