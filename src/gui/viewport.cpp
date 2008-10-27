8057f36623788c1d675e0311d98362b7d1d9f892
Ira Rice <irarice@gmail.com>
2008-10-27 06:05:18 +0000

    Changed mouse attack behavior so that if you click on a being that's too far away and are holding down the attack key, you will attack that being.

--- src/gui/viewport.cpp
+++ src/gui/viewport.cpp
@@ -300,7 +300,7 @@ void Viewport::mousePressed(gcn::MouseEvent &event)
                     if (being->mAction == Being::DEAD)
                         break;
 
-                    if (player_node->withinAttackRange(being))
+                    if (player_node->withinAttackRange(being) || keyboard.isKeyActive(keyboard.KEY_ATTACK))
                     {
                         player_node->setGotoTarget(being);
                         player_node->attack(being, keyboard.isKeyActive(keyboard.KEY_TARGET));
