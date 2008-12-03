72b42a7da7580e0eb52170f3d6c7d49dc0925e98
Ira Rice <irarice@gmail.com>
2008-12-03 12:38:29 -0700

    Added in the effects manager, which was accidently missing from the last commit.
    Signed-off-by: Ira Rice <irarice@gmail.com>

---
+++ src/effectmanager.cpp
@@ -0,0 +1,85 @@
+/*
+ *  The Mana World
+ *  Copyright 2004 The Mana World Development Team
+ *
+ *  This file is part of The Mana World.
+ *
+ *  The Mana World is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  The Mana World is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with The Mana World; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "effectmanager.h"
+
+#include "particle.h"
+#include "log.h"
+#include "sound.h"
+
+#include "utils/xml.h"
+
+
+EffectManager::EffectManager()
+{
+    XML::Document doc("effects.xml");
+    xmlNodePtr root = doc.rootNode();
+
+    if (!root || !xmlStrEqual(root->name, BAD_CAST "being-effects"))
+    {
+        logger->log("Error loading being effects file: effects.xml");
+        return;
+    }
+    else
+    {
+        logger->log("Effects are now loading");
+    } 
+
+    for_each_xml_child_node(node, root)
+    {
+        int id;
+
+        if (xmlStrEqual(node->name, BAD_CAST "effect"))
+        {
+            EffectDescription ed;
+            ed.id = XML::getProperty(node, "id", -1);
+            ed.GFX = XML::getProperty(node, "particle", "");
+            ed.SFX = XML::getProperty(node, "audio", "");
+            mEffects.push_back(ed);
+        }
+    }
+}
+
+EffectManager::~EffectManager()
+{
+
+}
+
+bool EffectManager::trigger(int id, int x, int y)
+{
+    bool rValue = false;
+    for (std::list<EffectDescription>::iterator i = mEffects.begin(); i != mEffects.end(); ++i)
+    {
+        if ((*i).id == id)
+        {
+            printf("Found effect, playing it");
+            rValue = true;
+            if((*i).GFX != "")
+                particleEngine->addEffect((*i).GFX, x, y);
+            if((*i).SFX != "")
+                sound.playSfx((*i).SFX);
+            break;            
+        }
+    }
+    return rValue;
+}
+
