015833d30d6890ab8693b334c419870685cb426f
Bjørn Lindeijer <bjorn@lindeijer.nl>
2008-12-07 15:33:35 +0100

    Updated version to 0.0.27

--- src/winver.h
+++ src/winver.h
@@ -1,6 +1,6 @@
 /* VERSION DEFINITIONS */
 #define VER_MAJOR 0
 #define VER_MINOR 0
-#define VER_RELEASE 26
+#define VER_RELEASE 27
 #define VER_BUILD 0
-#define PACKAGE_VERSION "0.0.26"
+#define PACKAGE_VERSION "0.0.27"
