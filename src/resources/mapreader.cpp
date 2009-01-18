7b38f31192be70463cfa1fa6f0ffe13181fc26a9
Philipp Sehmisch <crush@themanaworld.org>
2009-01-18 21:40:52 +0100

    Fixed mapreader crash caused by unknown tile properties (That's why I said that I would like some praxis-oriented testing of the animation system before the release).

--- src/resources/mapreader.cpp
+++ src/resources/mapreader.cpp
@@ -532,8 +532,12 @@ Tileset *MapReader::readTileset(xmlNodePtr node,
                         break;
                     }
                 }
-                map->addAnimation(tileGID, new TileAnimation(ani));
-                logger->log("Animation length: %d", ani->getLength());
+
+                if (ani->getLength() > 0)
+                {
+                    map->addAnimation(tileGID, new TileAnimation(ani));
+                    logger->log("Animation length: %d", ani->getLength());
+                }
             }
         }
     }
