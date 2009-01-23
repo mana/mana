7b64259cbdf2987159671c3226b42d902942b275
Bjørn Lindeijer <bjorn@lindeijer.nl>
2009-01-23 11:19:23 +0100

    Removed unnecessary references to The Mana World in code headers
    This dates back to the old days of TMW, but the usage instructions of
    GPLv2 don't mention this being necessary. Since it doesn't add anything,
    avoid the branding in these sections.

--- tools/tmxcopy/map.cpp
+++ tools/tmxcopy/map.cpp
@@ -1,20 +1,19 @@
 /*
  *  TMXCopy
- *  Copyright 2007 Philipp Sehmisch
+ *  Copyright (C) 2007  Philipp Sehmisch
  *
- *
- *  TMXCopy is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  any later version.
  *
- *  TMXCopy is distributed in the hope that it will be useful,
+ *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with TMXCopy; if not, write to the Free Software
+ *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
