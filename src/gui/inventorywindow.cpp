85a02d69af733b327e4f3bb8011472de95bbba07
Ira Rice <irarice@gmail.com>
2009-01-30 13:23:25 -0700

    Moved weight and slots back to the top of the inventory window.
    Signed-off-by: Ira Rice <irarice@gmail.com>

--- src/gui/inventorywindow.cpp
+++ src/gui/inventorywindow.cpp
@@ -91,16 +91,16 @@ InventoryWindow::InventoryWindow(int invSize):
     setMinHeight(130);
     setMinWidth(mWeightLabel->getWidth() + mSlotsLabel->getWidth() + 310);
 
-    place(0, 0, mInvenScroll, 7, 4);
-    place(0, 4, mWeightLabel).setPadding(3);
-    place(1, 4, mWeightBar, 2);
-    place(3, 4, mSlotsLabel).setPadding(3);
-    place(4, 4, mSlotsBar, 2);
+    place(0, 0, mWeightLabel).setPadding(3);
+    place(1, 0, mWeightBar, 2);
+    place(3, 0, mSlotsLabel).setPadding(3);
+    place(4, 0, mSlotsBar, 2);
+    place(0, 1, mInvenScroll, 7, 4);
     place(5, 5, mDropButton);
     place(6, 5, mUseButton);
 
     Layout &layout = getLayout();
-    layout.setRowHeight(0, Layout::AUTO_SET);
+    layout.setRowHeight(0,  mDropButton->getHeight());
 
     loadWindowState();
     setLocationRelativeTo(getParent());
