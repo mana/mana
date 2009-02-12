70aff3a9f745e23fcfa11db92563d0ed17449c32
MadCamel <madcamel@gmail.com>
2009-02-11 19:24:14 -0500

    Implemented /me

--- src/gui/chat.cpp
+++ src/gui/chat.cpp
@@ -138,6 +138,16 @@ void ChatWindow::chatLog(std::string line, int own, bool ignoreRecord)
         }
     }
 
+    // *implements actions in a backwards compatible way*
+    if (own == BY_PLAYER &&
+	tmp.text.at(0) == '*' &&
+	tmp.text.at(tmp.text.length()-1) == '*')
+    {
+	tmp.text[0] = ' ';
+	tmp.text.erase(tmp.text.length() - 1);
+	own = ACT_IS;
+    }
+
     std::string lineColor = "##C";
     switch (own)
     {
@@ -594,6 +604,12 @@ void ChatWindow::chatSend(const std::string &nick, std::string msg)
         {
             chatLog(_("Present: ") + response, BY_SERVER);
         }
+    }
+    else if (command == "me")
+    {
+	std::stringstream actionStr;
+	actionStr << "*" << msg << "*";
+	chatSend(player_node->getName(), actionStr.str());
     }
     else
     {
