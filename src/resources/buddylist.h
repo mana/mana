74f4c80b84b6f0ca934bb8f2a593ca0cea326325
Bjørn Lindeijer <bjorn@lindeijer.nl>
2009-03-26 00:55:12 +0100

    Fixes to file headers and header guards
    Mostly removal of branding for tmwserv related files, as was done for
    the eAthena client before.

--- src/resources/buddylist.h
+++ src/resources/buddylist.h
@@ -1,26 +1,26 @@
 /*
  *  The Mana World
- *  Copyright 2004 The Mana World Development Team
+ *  Copyright (C) 2004  The Mana World Development Team
  *
  *  This file is part of The Mana World.
  *
- *  The Mana World is free software; you can redistribute it and/or modify
+ *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  any later version.
  *
- *  The Mana World is distributed in the hope that it will be useful,
+ *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with The Mana World; if not, write to the Free Software
+ *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef _TMW_BUDDYLIST_H
-#define _TMW_BUDDYLIST_H
+#ifndef BUDDYLIST_H
+#define BUDDYLIST_H
 
 #include <list>
 #include <string>
@@ -76,4 +76,4 @@ class BuddyList : public gcn::ListModel {
         std::string mFilename;            /* File to work with */
 };
 
-#endif /* _TMW_BUDDYLIST_H */
+#endif /* BUDDYLIST_H */
