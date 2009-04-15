31f0b79de3e23f3660c35795232020870fb38590
Jared Adams <jaxad0127@gmail.com>
2009-04-14 20:50:14 -0600

    Add misisng mathutils files

---
+++ src/utils/mathutils.h
@@ -0,0 +1,31 @@
+/*
+ *  The Mana World
+ *  Copyright (C) 2009  The Mana World Development Team
+ *
+ *  This file is part of The Mana World.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef UTILS_MATHUTILS_H
+#define UTILS_MATHUTILS_H
+
+float fastInvSqrt(float x);
+
+float fastSqrt(float x);
+
+float weightedAverage(float n1, float n2, float w);
+
+#endif // UTILS_MATHUTILS_H
