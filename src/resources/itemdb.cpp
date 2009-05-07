24d218f216b57f28d428c88279d9f098f7a0acb0
Tametomo <irarice@gmail.com>
2009-05-07 03:09:04 -0600

    Don't complain in the logger about not having an image attribute for negative items, since these aren't inventory items.
    Signed-off-by: Tametomo <irarice@gmail.com>
    Signed-off-by: Bjørn Lindeijer <bjorn@lindeijer.nl>

--- src/resources/itemdb.cpp
+++ src/resources/itemdb.cpp
@@ -218,8 +218,8 @@ void ItemDB::load()
         {
             CHECK_PARAM(name, "");
             CHECK_PARAM(description, "");
+            CHECK_PARAM(image, "");
         }
-        CHECK_PARAM(image, "");
         // CHECK_PARAM(effect, "");
         // CHECK_PARAM(type, 0);
         // CHECK_PARAM(weight, 0);
