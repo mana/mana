f2c04a89d21e4a8a7e424fe49c8fea27ea6e10cb
Dennis Friis <peavey@inspircd.org>
2009-06-07 23:50:04 +0200

    Let athena dictate the walking speed of beings.
    This seems to have the added bones of mobs no longer being so much out
    of sync when you attack them. Also adds support for GM command @speed.

--- src/net/ea/playerhandler.cpp
+++ src/net/ea/playerhandler.cpp
@@ -223,9 +223,7 @@ void PlayerHandler::handleMessage(MessageIn &msg)
 
                 switch (type)
                 {
-                    //case 0x0000:
-                    //    player_node->setWalkSpeed(msg.readInt32());
-                    //    break;
+                    case 0x0000: player_node->setWalkSpeed(value); break;
                     case 0x0005: player_node->setHp(value); break;
                     case 0x0006: player_node->setMaxHp(value); break;
                     case 0x0007: player_node->mMp = value; break;
