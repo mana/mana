5000d1d38f18cbc0008cac2e73845bc618056f6e
Tametomo <irarice@gmail.com>
2009-06-12 14:04:29 -0600

    Fixed Viewport from acknowledging clicks from widgets on top of it when they happened within a contained widget.
    Signed-off-by: Tametomo <irarice@gmail.com>

--- src/gui/viewport.cpp
+++ src/gui/viewport.cpp
@@ -295,6 +295,9 @@ void Viewport::_drawPath(Graphics *graphics, const Path &path)
 
 void Viewport::mousePressed(gcn::MouseEvent &event)
 {
+    if (event.getSource() != this)
+        return;
+
     // Check if we are alive and kickin'
     if (!mMap || !player_node || !player_node->isAlive())
         return;
