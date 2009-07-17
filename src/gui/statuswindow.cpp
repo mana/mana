480830afdfb3d43d7550bfe3a842a23d18b8dded
Kess Vargavind <vargavind@gmail.com>
2009-07-17 15:17:15 +0200

    PLEASE CHECK: Compilation fix
    I am able to compile against tmwserv with this mini fix, but I am quite
    unsure what the code is supposed to look like.

--- src/gui/statuswindow.cpp
+++ src/gui/statuswindow.cpp
@@ -203,6 +203,10 @@ std::string StatusWindow::update(int id)
 {
     // TODO: only update what changed
     update();
+
+    // FIXME: Added the return value to be able to compile, haven't got a
+    //        clue what it is supposed to return (Kess, July 2009)
+    return "";
 }
 
 void StatusWindow::addAttribute(int id, const std::string &name,
