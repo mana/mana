01a35a4b243c288a3c5f74abbfc73204c9e45f54
Philipp Sehmisch <crush@themanaworld.org>
2009-08-30 16:40:17 +0200

    Fixed a style error.

--- src/gui/debugwindow.cpp
+++ src/gui/debugwindow.cpp
@@ -51,7 +51,7 @@ DebugWindow::DebugWindow():
     mMapLabel = new Label(strprintf(_("Map: %s"), ""));
     mMinimapLabel = new Label(strprintf(_("Minimap: %s"), ""));
     mTileMouseLabel = new Label(strprintf(_("Tile: (%d, %d)"), 0, 0));
-    mParticleCountLabel = new Label(strprintf(_("Particle count: _____"), 0));
+    mParticleCountLabel = new Label(strprintf(_("Particle count: _____")));
 
     place(0, 0, mFPSLabel, 3);
     place(3, 0, mTileMouseLabel);
