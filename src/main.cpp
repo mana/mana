mana/mana

commit d5d10a30ceb4a9d6bbb19fe8cbcf878cd841cd53

Support switching characters under eAthena

src/main.cpp
@@ -808,19 +808,9 @@ int main(int argc, char *argv[])
     setupWindow = new Setup;
 
     gcn::Container *top = static_cast<gcn::Container*>(gui->getTop());
-    Desktop *desktop = new Desktop;
-    top->add(desktop);
-    ProgressBar *progressBar = new ProgressBar(0.0f, 100, 20,
-                                               gcn::Color(168, 116, 31));
-    progressBar->setSmoothProgress(false);
-    gcn::Label *progressLabel = new Label;
-    top->add(progressBar, 5, top->getHeight() - 5 - progressBar->getHeight());
-    top->add(progressLabel, 15 + progressBar->getWidth(),
-                            progressBar->getY() + 4);
-    progressBar->setVisible(false);
-    gcn::Button *setupButton = new Button(_("Setup"), "Setup", &listener);
-    setupButton->setPosition(top->getWidth() - setupButton->getWidth() - 3, 3);
-    top->add(setupButton);
+    Desktop *desktop;
+    ProgressBar *progressBar;
+    Button *setupButton;
 
     sound.playMusic(branding.getValue("loginMusic", "Magick - Real.ogg"));
 
@@ -844,12 +834,6 @@ int main(int argc, char *argv[])
     if (loginData.username.empty() && loginData.remember) {
         loginData.username = config.getValue("username", "");
     }
-
-    int screenWidth = (int) config.getValue("screenwidth", defaultScreenWidth);
-    int screenHeight = static_cast<int>(config.getValue("screenheight",
-                                                        defaultScreenHeight));
-
-    desktop->setSize(screenWidth, screenHeight);
 
     if (state != STATE_ERROR)
         state = STATE_CHOOSE_SERVER;
@@ -876,7 +860,7 @@ int main(int argc, char *argv[])
                     if (event.key.keysym.sym == SDLK_ESCAPE)
                     {
                         if (!quitDialog)
-                            quitDialog = new QuitDialog(NULL, &quitDialog);
+                            quitDialog = new QuitDialog(&quitDialog);
                         else
                             quitDialog->requestMoveToTop();
                     }
@@ -921,6 +905,36 @@ int main(int argc, char *argv[])
         {
             Net::getCharHandler()->setCharInfo(&charInfo);
             state = STATE_LOGIN;
+        }
+        else if (oldstate == STATE_START || oldstate == STATE_GAME)
+        {
+            desktop = new Desktop;
+            top->add(desktop);
+            progressBar = new ProgressBar(0.0f, 100, 20,
+                                          gcn::Color(168, 116, 31));
+            progressBar->setSmoothProgress(false);
+            Label *progressLabel = new Label;
+            top->add(progressBar, 5, top->getHeight() - 5 -
+                     progressBar->getHeight());
+            top->add(progressLabel, 15 + progressBar->getWidth(),
+                     progressBar->getY() + 4);
+            progressBar->setVisible(false);
+            setupButton = new Button(_("Setup"), "Setup", &listener);
+            setupButton->setPosition(top->getWidth() - setupButton->getWidth()
+                                     - 3, 3);
+            top->add(setupButton);
+
+            int screenWidth = (int) config.getValue("screenwidth",
+                                                    defaultScreenWidth);
+            int screenHeight = (int) config.getValue("screenheight",
+                                                     defaultScreenHeight);
+
+            desktop->setSize(screenWidth, screenHeight);
+        }
+
+        if (state == STATE_SWITCH_LOGIN && oldstate == STATE_GAME)
+        {
+            Net::getGameHandler()->clear();
         }
 
         if (state != oldstate)
@@ -1135,9 +1149,8 @@ int main(int argc, char *argv[])
                     delete game;
                     game = 0;
 
-                    state = STATE_EXIT;
-
-                    Net::getGeneralHandler()->unload();
+                    if (state == STATE_GAME)
+                        state = STATE_EXIT;
 
                     break;
 
@@ -1271,6 +1284,7 @@ int main(int argc, char *argv[])
 
                 case STATE_EXIT:
                     logger->log("State: EXIT");
+                    Net::getGeneralHandler()->unload();
                     break;
 
                 case STATE_FORCE_QUIT:

src/game.cpp
@@ -97,7 +97,6 @@
 
 std::string map_path;
 
-bool done = false;
 volatile int tick_time;
 volatile int fps = 0, frame = 0;
 
@@ -166,7 +165,7 @@ namespace {
         void action(const gcn::ActionEvent &event)
         {
             if (event.getId() == "yes" || event.getId() == "ok")
-                done = true;
+                state = STATE_EXIT;
 
             disconnectedDialog = NULL;
         }
@@ -274,6 +273,8 @@ static void createGuiWindows()
     Net::getGeneralHandler()->guiWindowsLoaded();
 }
 
+#define del_0(X) { delete X; X = 0; }
+
 /**
  * Destroy all the globally accessible gui windows
  */
@@ -281,42 +282,40 @@ static void destroyGuiWindows()
 {
     Net::getGeneralHandler()->guiWindowsUnloaded();
     logger->setChatWindow(NULL);
-    delete localChatTab; // Need to do this first, so it can remove itself
-    delete chatWindow;
-    delete statusWindow;
-    delete miniStatusWindow;
-    delete buyDialog;
-    delete sellDialog;
+    del_0(localChatTab); // Need to do this first, so it can remove itself
+    del_0(chatWindow);
+    del_0(statusWindow);
+    del_0(miniStatusWindow);
+    del_0(buyDialog);
+    del_0(sellDialog);
 #ifdef EATHENA_SUPPORT
-    delete buySellDialog;
+    del_0(buySellDialog);
 #endif
-    delete inventoryWindow;
-    delete partyWindow;
-    delete npcDialog;
-    delete npcPostDialog;
+    del_0(inventoryWindow);
+    del_0(partyWindow);
+    del_0(npcDialog);
+    del_0(npcPostDialog);
 #ifdef MANASERV_SUPPORT
-    delete buddyWindow;
-    delete guildWindow;
+    del_0(buddyWindow);
+    del_0(guildWindow);
 #endif
-    delete skillDialog;
-    delete minimap;
-    delete equipmentWindow;
-    delete tradeWindow;
-    delete helpWindow;
-    delete debugWindow;
-    delete itemShortcutWindow;
-    delete emoteShortcutWindow;
-    delete storageWindow;
-    delete outfitWindow;
-    delete specialsWindow;
+    del_0(skillDialog);
+    del_0(minimap);
+    del_0(equipmentWindow);
+    del_0(tradeWindow);
+    del_0(helpWindow);
+    del_0(debugWindow);
+    del_0(itemShortcutWindow);
+    del_0(emoteShortcutWindow);
+    del_0(storageWindow);
+    del_0(outfitWindow);
+    del_0(specialsWindow);
 }
 
 Game::Game():
     mLastTarget(Being::UNKNOWN),
     mLogicCounterId(0), mSecondsCounterId(0)
 {
-    done = false;
-
     createGuiWindows();
 
     mWindowMenu = new WindowMenu;
@@ -454,7 +453,7 @@ void Game::logic()
     int gameTime = tick_time;
     mDrawTime = tick_time * MILLISECONDS_IN_A_TICK;
 
-    while (!done)
+    while (state == STATE_GAME)
     {
         if (Map *map = engine->getCurrentMap())
             map->update(get_elapsed_time(gameTime));
@@ -738,7 +737,7 @@ void Game::handleInput()
                case KeyboardConfig::KEY_QUIT:
                     if (!quitDialog)
                     {
-                        quitDialog = new QuitDialog(&done, &quitDialog);
+                        quitDialog = new QuitDialog(&quitDialog);
                         quitDialog->requestMoveToTop();
                     }
                     else
@@ -915,7 +914,7 @@ void Game::handleInput()
         // Quit event
         else if (event.type == SDL_QUIT)
         {
-            done = true;
+            state = STATE_EXIT;
         }
 
         // Push input to GUI when not used

src/gui/quitdialog.cpp
@@ -26,10 +26,15 @@
 #include "gui/widgets/button.h"
 #include "gui/widgets/radiobutton.h"
 
+#include "net/charhandler.h"
+#include "net/net.h"
+
 #include "utils/gettext.h"
 
-QuitDialog::QuitDialog(bool* quitGame, QuitDialog** pointerToMe):
-    Window(_("Quit"), true, NULL), mQuitGame(quitGame), mMyPointer(pointerToMe)
+#include <assert.h>
+
+QuitDialog::QuitDialog(QuitDialog** pointerToMe):
+    Window(_("Quit"), true, NULL), mMyPointer(pointerToMe)
 {
 
     mLogoutQuit = new RadioButton(_("Quit"), "quitdialog");
@@ -104,27 +109,17 @@ void QuitDialog::action(const gcn::ActionEvent &event)
         }
         else if (mLogoutQuit->isSelected())
         {
-            if ((state == STATE_GAME) && (mQuitGame))
-            {
-                *mQuitGame = true;
-            }
-
             state = STATE_EXIT;
         }
         else if (mSwitchAccountServer->isSelected())
         {
-            if ((state == STATE_GAME) && (mQuitGame))
-            {
-                *mQuitGame = true;
-            }
-
             state = STATE_SWITCH_SERVER;
         }
         else if (mSwitchCharacter->isSelected())
         {
-            if (mQuitGame) *mQuitGame = true;
+            assert(state == STATE_GAME);
 
-            state = STATE_SWITCH_CHARACTER;
+            Net::getCharHandler()->switchCharacter();
         }
     }
     scheduleDelete();

src/gui/quitdialog.h
@@ -43,7 +43,7 @@ class QuitDialog : public Window, public gcn::ActionListener
          * @quitGame;    to be used for getting out of the while loop in Game
          * @pointerToMe  will be set to NULL when the QuitDialog is destroyed
          */
-        QuitDialog(bool *quitGame, QuitDialog **pointerToMe);
+        QuitDialog(QuitDialog **pointerToMe);
 
         /**
          * Destructor
@@ -63,7 +63,6 @@ class QuitDialog : public Window, public gcn::ActionListener
         gcn::Button *mOkButton;
         gcn::Button *mCancelButton;
 
-        bool *mQuitGame;
         QuitDialog **mMyPointer;
 };
 

src/gui/statuswindow.cpp
@@ -183,6 +183,10 @@ StatusWindow::StatusWindow():
 
 std::string StatusWindow::update(int id)
 {
+     /*/ TODO get rid of this
+    if (!player_node)
+        return "";*/
+
     if (miniStatusWindow)
         miniStatusWindow->update(id);
 

src/net/charhandler.h
@@ -52,7 +52,9 @@ class CharHandler
                         bool gender, int hairstyle, int hairColor,
                         std::vector<int> stats) = 0;
 
-        virtual void deleteCharacter(int slot, LocalPlayer *character) = 0;
+        virtual void deleteCharacter(int slot, LocalPlayer* character) = 0;
+
+        virtual void switchCharacter() = 0;
 };
 
 } // namespace Net

src/net/ea/beinghandler.cpp
@@ -118,6 +118,9 @@ void BeingHandler::handleMessage(MessageIn &msg)
     {
         case SMSG_BEING_VISIBLE:
         case SMSG_BEING_MOVE:
+            if (!beingManager)
+                return;
+
             // Information about a being in range
             id = msg.readInt32();
             speed = msg.readInt16();

src/net/ea/charserverhandler.cpp
@@ -284,6 +284,13 @@ void CharServerHandler::deleteCharacter(int slot, LocalPlayer *character)
     outMsg.writeString("a@a.com", 40);
 }
 
+void CharServerHandler::switchCharacter()
+{
+    // This is really a map-server packet
+    MessageOut outMsg(CMSG_PLAYER_RESTART);
+    outMsg.writeInt8(1);
+}
+
 void CharServerHandler::connect()
 {
     const Token &token =

commit d93c47be924815883c96d089cad5deba2bf381fc

Fixed crash on startup
Was introduced in d5d10a30ceb4a9d6bbb19fe8cbcf878cd841cd53. In
particular, the progress bar was not initialized to 0, even though it is
checked for 0 later on and used if it isn't 0.

Thanks to 4144 for reporting this issue.

src/main.cpp
@@ -808,9 +808,9 @@ int main(int argc, char *argv[])
     setupWindow = new Setup;
 
     gcn::Container *top = static_cast<gcn::Container*>(gui->getTop());
-    Desktop *desktop;
-    ProgressBar *progressBar;
-    Button *setupButton;
+    Desktop *desktop = 0;
+    ProgressBar *progressBar = 0;
+    Button *setupButton = 0;
 
     sound.playMusic(branding.getValue("loginMusic", "Magick - Real.ogg"));
 