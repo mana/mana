Tametomo/Aethyra

Allow handling drag events in tables as well.
Signed-off-by: Tametomo <irarice@gmail.com>

src/bindings/guichan/widgets/table.cpp
@@ -462,9 +462,18 @@ void Table::mouseWheelMovedDown(gcn::MouseEvent& mouseEvent)
         mouseEvent.consume();
     }
 }
-        
-void Table::mouseDragged(gcn::MouseEvent& mouseEvent)
+
+void Table::mouseDragged(gcn::MouseEvent &event)
 {
+    if (event.getButton() != gcn::MouseEvent::LEFT)
+        return;
+
+    // Make table selection update on drag
+    const int x = std::max(0, event.getX());
+    const int y = std::max(0, event.getY());
+
+    setSelectedRow(getRowForY(y));
+    setSelectedColumn(getColumnForX(x));
 }
 
 // -- TableModelListener notifications

mana/mana

src/gui/widgets/table.cpp
@@ -462,6 +462,15 @@ void GuiTable::mouseWheelMovedDown(gcn::MouseEvent& mouseEvent)
 
 void GuiTable::mouseDragged(gcn::MouseEvent& mouseEvent)
 {
+    if (mouseEvent.getButton() != gcn::MouseEvent::LEFT)
+        return;
+
+    // Make table selection update on drag
+    const int x = std::max(0, mouseEvent.getX());
+    const int y = std::max(0, mouseEvent.getY());
+
+    setSelectedRow(getRowForY(y));
+    setSelectedColumn(getColumnForX(x));
 }
 
 // -- TableModelListener notifications