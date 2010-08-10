fa2cf9bea837e17d08218d0070a60905adec268f
Jared Adams <jaxad0127@gmail.com>
2010-08-09 18:48:57 -0600

    Fix enet version check *again*
    Reviewed-by: Chuck Miller

--- src/net/manaserv/connection.h
+++ src/net/manaserv/connection.h
@@ -26,6 +26,12 @@
 
 #include <iosfwd>
 
+#ifdef ENET_VERSION_CREATE
+#define ENET_CUTOFF ENET_VERSION_CREATE(1,3,0)
+#else
+#define ENET_CUTOFF 0xFFFFFFFF
+#endif
+
 namespace ManaServ
 {
     class MessageOut;
