0551d51a369b93b668496f0ab179cb5db7b01f44
Yohann Ferreira <yohann_dot_ferreira_at_orange_dot_efer>
2011-09-09 02:58:41 +0200

    Set the current build version to 0.6.0 in prevision of a release.

--- src/winver.h
+++ src/winver.h
@@ -1,6 +1,6 @@
 /* VERSION DEFINITIONS */
-#define VER_MAJOR 1
-#define VER_MINOR 0
+#define VER_MAJOR 0
+#define VER_MINOR 6
 #define VER_RELEASE 0
 #define VER_BUILD 0
-#define PACKAGE_VERSION "1.0.0.0"
+#define PACKAGE_VERSION "0.6.0.0"
