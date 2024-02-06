mana/mana

commit 2c51c98625b225cecfb9628c30d62d4e30f7e3e1

Ported to SDL2

src/graphics.cpp
@@ -19,8 +19,6 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <cassert>
-
 #include "graphics.h"
 #include "log.h"
 
@@ -28,103 +26,95 @@
 
 #include "utils/gettext.h"
 
-#include <SDL_gfxBlitFunc.h>
-
-Graphics::Graphics():
-    mWidth(0),
-    mHeight(0),
-    mBpp(0),
-    mFullscreen(false),
-    mHWAccel(false),
-    mBlitMode(BLIT_NORMAL)
-{
-}
+#include <guichan/exception.hpp>
 
 Graphics::~Graphics()
 {
     _endDraw();
 }
 
-bool Graphics::setVideoMode(int w, int h, int bpp, bool fs, bool hwaccel)
+void Graphics::setTarget(SDL_Window *target)
+{
+    _endDraw();
+
+    mTarget = target;
+
+    if (mTarget)
+        _beginDraw();
+}
+
+bool Graphics::setVideoMode(int w, int h, bool fs)
 {
     logger->log("Setting video mode %dx%d %s",
             w, h, fs ? "fullscreen" : "windowed");
 
-    logger->log("Bits per pixel: %d", bpp);
-
-    int displayFlags = SDL_ANYFORMAT;
+    int windowFlags = SDL_WINDOW_ALLOW_HIGHDPI;
 
     if (fs)
-        displayFlags |= SDL_FULLSCREEN;
+        windowFlags |= SDL_WINDOW_FULLSCREEN;
     else
-        displayFlags |= SDL_RESIZABLE;
+        windowFlags |= SDL_WINDOW_RESIZABLE;
 
-    if (hwaccel)
-        displayFlags |= SDL_HWSURFACE | SDL_DOUBLEBUF;
-    else
-        displayFlags |= SDL_SWSURFACE;
+    // TODO_SDL2: Support SDL_WINDOW_FULLSCREEN_DESKTOP
 
-    setTarget(SDL_SetVideoMode(w, h, bpp, displayFlags));
+    SDL_Window *window = nullptr;
+    SDL_Renderer *renderer = nullptr;
+    SDL_CreateWindowAndRenderer(w, h, windowFlags, &window, &renderer);
 
-    if (!mTarget)
+    if (!window)
         return false;
 
+    SDL_SetWindowMinimumSize(window, 640, 480);
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+    setTarget(window);
+
+    mRenderer = renderer;
     mWidth = w;
     mHeight = h;
-    mBpp = bpp;
     mFullscreen = fs;
-    mHWAccel = hwaccel;
 
-    char videoDriverName[64];
-
-    if (SDL_VideoDriverName(videoDriverName, 64))
-        logger->log("Using video driver: %s", videoDriverName);
+    if (const char *driver = SDL_GetCurrentVideoDriver())
+        logger->log("Using video driver: %s", driver);
     else
-        logger->log("Using video driver: unknown");
+        logger->log("Using video driver: not initialized");
 
-    const SDL_VideoInfo *vi = SDL_GetVideoInfo();
+    SDL_RendererInfo info;
 
-    logger->log("Possible to create hardware surfaces: %s",
-                vi->hw_available ? "yes" : "no");
-    logger->log("Window manager available: %s",
-                vi->wm_available ? "yes" : "no");
-    logger->log("Accelerated hardware to hardware blits: %s",
-                vi->blit_hw ? "yes" : "no");
-    logger->log("Accelerated hardware to hardware colorkey blits: %s",
-                vi->blit_hw_CC ? "yes" : "no");
-    logger->log("Accelerated hardware to hardware alpha blits: %s",
-                vi->blit_hw_A ? "yes" : "no");
-    logger->log("Accelerated software to hardware blits: %s",
-                vi->blit_sw ? "yes" : "no");
-    logger->log("Accelerated software to hardware colorkey blits: %s",
-                vi->blit_sw_CC ? "yes" : "no");
-    logger->log("Accelerated software to hardware alpha blits: %s",
-                vi->blit_sw_A ? "yes" : "no");
-    logger->log("Accelerated color fills: %s",
-                vi->blit_fill ? "yes" : "no");
-    logger->log("Available video memory: %d", vi->video_mem);
+    if (SDL_GetRendererInfo(mRenderer, &info) == 0) {
+        logger->log("Using renderer: %s", info.name);
+
+        logger->log("The renderer is a software fallback: %s",
+                    (info.flags & SDL_RENDERER_SOFTWARE) ? "yes" : "no");
+        logger->log("The renderer is hardware accelerated: %s",
+                    (info.flags & SDL_RENDERER_ACCELERATED) ? "yes" : "no");
+        logger->log("Vsync: %s",
+                    (info.flags & SDL_RENDERER_PRESENTVSYNC) ? "on" : "off");
+        logger->log("Renderer supports rendering to texture: %s",
+                    (info.flags & SDL_RENDERER_TARGETTEXTURE) ? "yes" : "no");
+        logger->log("Max texture size: %dx%d",
+                    info.max_texture_width, info.max_texture_height);
+    }
 
     return true;
 }
 
-bool Graphics::changeVideoMode(int w, int h, int bpp, bool fs, bool hwaccel)
+bool Graphics::changeVideoMode(int w, int h, bool fs)
 {
     // Just return success if we're already in this mode
     if (mWidth == w &&
             mHeight == h &&
-            mBpp == bpp &&
-            mFullscreen == fs &&
-            mHWAccel == hwaccel)
+            mFullscreen == fs)
         return true;
 
     _endDraw();
 
-    bool success = setVideoMode(w, h, bpp, fs, hwaccel);
+    bool success = setVideoMode(w, h, fs);
 
     // If it didn't work, try to restore the previous mode. If that doesn't
     // work either, we're in big trouble and bail out.
     if (!success) {
-        if (!setVideoMode(mWidth, mHeight, mBpp, mFullscreen, mHWAccel)) {
+        if (!setVideoMode(mWidth, mHeight, mFullscreen)) {
             logger->error(_("Failed to change video mode and couldn't "
                             "switch back to the previous mode!"));
         }
@@ -133,6 +123,12 @@ bool Graphics::changeVideoMode(int w, int h, int bpp, bool fs, bool hwaccel)
     _beginDraw();
 
     return success;
+}
+
+void Graphics::videoResized(int w, int h)
+{
+    mWidth = w;
+    mHeight = h;
 }
 
 int Graphics::getWidth() const
@@ -147,10 +143,10 @@ int Graphics::getHeight() const
 
 bool Graphics::drawImage(Image *image, int x, int y)
 {
-    if (image)
-        return drawImage(image, 0, 0, x, y, image->mBounds.w, image->mBounds.h);
-    else
+    if (!image)
         return false;
+
+    return drawImage(image, 0, 0, x, y, image->mBounds.w, image->mBounds.h);
 }
 
 bool Graphics::drawRescaledImage(Image *image, int srcX, int srcY,
@@ -160,15 +156,7 @@ bool Graphics::drawRescaledImage(Image *image, int srcX, int srcY,
                                bool useColor)
 {
     // Check that preconditions for blitting are met.
-    if (!mTarget || !image)
-        return false;
-    if (!image->mSDLSurface)
-        return false;
-
-    Image *tmpImage = image->SDLgetScaledImage(desiredWidth, desiredHeight);
-    bool returnValue = false;
-
-    if (!tmpImage)
+    if (!mTarget || !image || !image->mTexture)
         return false;
 
     dstX += mClipStack.top().xOffset;
@@ -183,76 +171,35 @@ bool Graphics::drawRescaledImage(Image *image, int srcX, int srcY,
     srcRect.x = srcX; srcRect.y = srcY;
     srcRect.w = width;
     srcRect.h = height;
+    dstRect.w = desiredWidth;
+    dstRect.h = desiredHeight;
 
-    returnValue = !(SDL_BlitSurface(tmpImage->mSDLSurface, &srcRect,
-                                    mTarget, &dstRect) < 0);
-
-    tmpImage->decRef(Resource::DeleteImmediately);
-
-    return returnValue;
+    return !(SDL_RenderCopy(mRenderer, image->mTexture, &srcRect, &dstRect) < 0);
 }
 
 bool Graphics::drawImage(Image *image, int srcX, int srcY, int dstX, int dstY,
-                         int width, int height, bool)
+                         int width, int height, bool useColor)
 {
-    // Check that preconditions for blitting are met.
-    if (!mTarget || !image || !image->mSDLSurface)
+    if (!image)
         return false;
 
-    dstX += mClipStack.top().xOffset;
-    dstY += mClipStack.top().yOffset;
-
-    srcX += image->mBounds.x;
-    srcY += image->mBounds.y;
-
-    SDL_Rect dstRect;
-    SDL_Rect srcRect;
-    dstRect.x = dstX; dstRect.y = dstY;
-    srcRect.x = srcX; srcRect.y = srcY;
-    srcRect.w = width;
-    srcRect.h = height;
-
-    if (mBlitMode == BLIT_NORMAL)
-        return !(SDL_BlitSurface(image->mSDLSurface, &srcRect, mTarget, &dstRect) < 0);
-    else
-        return !(SDL_gfxBlitRGBA(image->mSDLSurface, &srcRect, mTarget, &dstRect) < 0);
+    return drawRescaledImage(image,
+                             srcX, srcY,
+                             dstX, dstY,
+                             width, height,
+                             width, height, useColor);
 }
 
 void Graphics::drawImagePattern(Image *image, int x, int y, int w, int h)
 {
     // Check that preconditions for blitting are met.
-    if (!mTarget || !image)
-        return;
-    if (!image->mSDLSurface)
+    if (!image)
         return;
 
     const int iw = image->getWidth();
     const int ih = image->getHeight();
 
-    if (iw == 0 || ih == 0)
-        return;
-
-    for (int py = 0; py < h; py += ih)     // Y position on pattern plane
-    {
-        int dh = (py + ih >= h) ? h - py : ih;
-        int srcY = image->mBounds.y;
-        int dstY = y + py + mClipStack.top().yOffset;
-
-        for (int px = 0; px < w; px += iw) // X position on pattern plane
-        {
-            int dw = (px + iw >= w) ? w - px : iw;
-            int srcX = image->mBounds.x;
-            int dstX = x + px + mClipStack.top().xOffset;
-
-            SDL_Rect dstRect;
-            SDL_Rect srcRect;
-            dstRect.x = dstX; dstRect.y = dstY;
-            srcRect.x = srcX; srcRect.y = srcY;
-            srcRect.w = dw;   srcRect.h = dh;
-
-            SDL_BlitSurface(image->mSDLSurface, &srcRect, mTarget, &dstRect);
-        }
-    }
+    drawRescaledImagePattern(image, x, y, w, h, iw, ih);
 }
 
 void Graphics::drawRescaledImagePattern(Image *image,
@@ -261,50 +208,34 @@ void Graphics::drawRescaledImagePattern(Image *image,
                                         int scaledWidth, int scaledHeight)
 {
     // Check that preconditions for blitting are met.
-    if (!mTarget || !image)
-        return;
-    if (!image->mSDLSurface)
+    if (!mTarget || !image || !image->mTexture)
         return;
 
     if (scaledHeight == 0 || scaledWidth == 0)
         return;
 
-    Image *tmpImage = image->SDLgetScaledImage(scaledWidth, scaledHeight);
-    if (!tmpImage)
-        return;
-
-    const int iw = tmpImage->getWidth();
-    const int ih = tmpImage->getHeight();
-
-    if (iw == 0 || ih == 0)
+    for (int py = 0; py < h; py += scaledHeight)     // Y position on pattern plane
     {
-        tmpImage->decRef(Resource::DeleteImmediately);
-        return;
-    }
-
-    for (int py = 0; py < h; py += ih)     // Y position on pattern plane
-    {
-        int dh = (py + ih >= h) ? h - py : ih;
-        int srcY = tmpImage->mBounds.y;
+        int dh = (py + scaledHeight >= h) ? h - py : scaledHeight;
+        int srcY = image->mBounds.y;
         int dstY = y + py + mClipStack.top().yOffset;
 
-        for (int px = 0; px < w; px += iw) // X position on pattern plane
+        for (int px = 0; px < w; px += scaledWidth) // X position on pattern plane
         {
-            int dw = (px + iw >= w) ? w - px : iw;
-            int srcX = tmpImage->mBounds.x;
+            int dw = (px + scaledWidth >= w) ? w - px : scaledWidth;
+            int srcX = image->mBounds.x;
             int dstX = x + px + mClipStack.top().xOffset;
 
             SDL_Rect dstRect;
             SDL_Rect srcRect;
             dstRect.x = dstX; dstRect.y = dstY;
+            dstRect.w = dw;   dstRect.h = dh;
             srcRect.x = srcX; srcRect.y = srcY;
             srcRect.w = dw;   srcRect.h = dh;
 
-            SDL_BlitSurface(tmpImage->mSDLSurface, &srcRect, mTarget, &dstRect);
+            SDL_RenderCopy(mRenderer, image->mTexture, &srcRect, &dstRect);
         }
     }
-
-    tmpImage->decRef(Resource::DeleteImmediately);
 }
 
 void Graphics::drawImageRect(int x, int y, int w, int h,
@@ -361,7 +292,7 @@ void Graphics::drawImageRect(int x, int y, int w, int h,
 
 void Graphics::updateScreen()
 {
-    SDL_Flip(mTarget);
+    SDL_RenderPresent(mRenderer);
 }
 
 SDL_Surface *Graphics::getScreenshot()
@@ -377,10 +308,140 @@ SDL_Surface *Graphics::getScreenshot()
 #endif
     int amask = 0x00000000;
 
-    SDL_Surface *screenshot = SDL_CreateRGBSurface(SDL_SWSURFACE, mTarget->w,
-            mTarget->h, 24, rmask, gmask, bmask, amask);
-
-    SDL_BlitSurface(mTarget, NULL, screenshot, NULL);
+    SDL_Surface *screenshot = SDL_CreateRGBSurface(0, mWidth,
+            mHeight, 24, rmask, gmask, bmask, amask);
+    SDL_RenderReadPixels(mRenderer, NULL, SDL_PIXELFORMAT_RGB888, screenshot->pixels, screenshot->pitch);
 
     return screenshot;
+}
+
+bool Graphics::pushClipArea(gcn::Rectangle area)
+{
+    bool result = gcn::Graphics::pushClipArea(area);
+    updateSDLClipRect();
+    return result;
+}
+
+void Graphics::popClipArea()
+{
+    gcn::Graphics::popClipArea();
+    updateSDLClipRect();
+}
+
+void Graphics::updateSDLClipRect()
+{
+    if (mClipStack.empty())
+    {
+        SDL_RenderSetClipRect(mRenderer, NULL);
+        return;
+    }
+
+    const gcn::ClipRectangle &carea = mClipStack.top();
+    SDL_Rect rect;
+    rect.x = carea.x;
+    rect.y = carea.y;
+    rect.w = carea.width;
+    rect.h = carea.height;
+
+    SDL_RenderSetClipRect(mRenderer, &rect);
+}
+
+void Graphics::drawPoint(int x, int y)
+{
+    if (mClipStack.empty())
+    {
+        throw GCN_EXCEPTION("Clip stack is empty, perhaps you called a draw funtion outside of _beginDraw() and _endDraw()?");
+    }
+
+    const gcn::ClipRectangle &top = mClipStack.top();
+
+    x += top.xOffset;
+    y += top.yOffset;
+
+    if (!top.isPointInRect(x, y))
+        return;
+
+    SDL_SetRenderDrawColor(mRenderer,
+                           (Uint8)(mColor.r),
+                           (Uint8)(mColor.g),
+                           (Uint8)(mColor.b),
+                           (Uint8)(mColor.a));
+    SDL_RenderDrawPoint(mRenderer, x, y);
+}
+
+void Graphics::drawLine(int x1, int y1, int x2, int y2)
+{
+    if (mClipStack.empty())
+    {
+        throw GCN_EXCEPTION("Clip stack is empty, perhaps you called a draw funtion outside of _beginDraw() and _endDraw()?");
+    }
+
+    const gcn::ClipRectangle &top = mClipStack.top();
+
+    x1 += top.xOffset;
+    y1 += top.yOffset;
+    x2 += top.xOffset;
+    y2 += top.yOffset;
+
+    SDL_SetRenderDrawColor(mRenderer,
+                           (Uint8)(mColor.r),
+                           (Uint8)(mColor.g),
+                           (Uint8)(mColor.b),
+                           (Uint8)(mColor.a));
+    SDL_RenderDrawLine(mRenderer, x1, y1, x2, y2);
+}
+
+void Graphics::drawRectangle(const gcn::Rectangle &rectangle)
+{
+    if (mClipStack.empty())
+    {
+        throw GCN_EXCEPTION("Clip stack is empty, perhaps you called a draw funtion outside of _beginDraw() and _endDraw()?");
+    }
+
+    const gcn::ClipRectangle &top = mClipStack.top();
+
+    SDL_Rect rect;
+    rect.x = rectangle.x + top.xOffset;
+    rect.y = rectangle.y + top.yOffset;
+    rect.w = rectangle.width;
+    rect.h = rectangle.height;
+
+    SDL_SetRenderDrawColor(mRenderer,
+                           (Uint8)(mColor.r),
+                           (Uint8)(mColor.g),
+                           (Uint8)(mColor.b),
+                           (Uint8)(mColor.a));
+    SDL_RenderDrawRect(mRenderer, &rect);
+}
+
+void Graphics::fillRectangle(const gcn::Rectangle &rectangle)
+{
+    if (mClipStack.empty())
+    {
+        throw GCN_EXCEPTION("Clip stack is empty, perhaps you called a draw funtion outside of _beginDraw() and _endDraw()?");
+    }
+
+    const gcn::ClipRectangle &top = mClipStack.top();
+
+    gcn::Rectangle area = rectangle;
+    area.x += top.xOffset;
+    area.y += top.yOffset;
+
+    if(!area.isIntersecting(top))
+    {
+        return;
+    }
+
+    SDL_Rect rect;
+    rect.x = area.x;
+    rect.y = area.y;
+    rect.w = area.width;
+    rect.h = area.height;
+
+    SDL_SetRenderDrawColor(mRenderer,
+                           (Uint8)(mColor.r),
+                           (Uint8)(mColor.g),
+                           (Uint8)(mColor.b),
+                           (Uint8)(mColor.a));
+    SDL_RenderFillRect(mRenderer, &rect);
 }

CMakeLists.txt
@@ -1,5 +1,5 @@
-CMAKE_MINIMUM_REQUIRED(VERSION 2.6)
-CMAKE_POLICY(VERSION 2.6)
+CMAKE_MINIMUM_REQUIRED(VERSION 2.8.12)
+CMAKE_POLICY(VERSION 2.8.12)
 
 PROJECT(MANA)
 

src/CMakeLists.txt
@@ -1,9 +1,10 @@
-FIND_PACKAGE(SDL REQUIRED)
-FIND_PACKAGE(SDL_image REQUIRED)
-FIND_PACKAGE(SDL_mixer REQUIRED)
-FIND_PACKAGE(SDL_net REQUIRED)
-FIND_PACKAGE(SDL_ttf REQUIRED)
-FIND_PACKAGE(SDL_gfx REQUIRED)
+include(FindPkgConfig)
+pkg_check_modules(SDL2 REQUIRED sdl2>=2.0.1)
+pkg_check_modules(SDL2IMAGE REQUIRED SDL2_image)
+pkg_check_modules(SDL2MIXER REQUIRED SDL2_mixer)
+pkg_check_modules(SDL2NET REQUIRED SDL2_net)
+pkg_check_modules(SDL2TTF REQUIRED SDL2_ttf)
+pkg_check_modules(SDL2GFX REQUIRED SDL2_gfx)
 FIND_PACKAGE(CURL REQUIRED)
 FIND_PACKAGE(LibXml2 REQUIRED)
 FIND_PACKAGE(PhysFS REQUIRED)
@@ -69,8 +70,7 @@ ELSEIF (CMAKE_SYSTEM_NAME STREQUAL SunOS)
     SET(EXTRA_LIBRARIES intl)
 ENDIF()
 
-SET(GUICHAN_COMPONENTS "SDL")
-FIND_PACKAGE(Guichan REQUIRED ${GUICHAN_COMPONENTS})
+FIND_PACKAGE(Guichan REQUIRED)
 
 IF (WITH_OPENGL)
     FIND_PACKAGE(OpenGL REQUIRED)
@@ -87,12 +87,12 @@ ENDIF (USE_X11)
 
 INCLUDE_DIRECTORIES(
     ${CMAKE_CURRENT_SOURCE_DIR}
-    ${SDL_INCLUDE_DIR}
-    ${SDLIMAGE_INCLUDE_DIR}
-    ${SDLMIXER_INCLUDE_DIR}
-    ${SDLNET_INCLUDE_DIR}
-    ${SDLTTF_INCLUDE_DIR}
-    ${SDLGFX_INCLUDE_DIR}
+    ${SDL2_INCLUDE_DIRS}
+    ${SDL2IMAGE_INCLUDE_DIRS}
+    ${SDL2MIXER_INCLUDE_DIRS}
+    ${SDL2NET_INCLUDE_DIRS}
+    ${SDL2TTF_INCLUDE_DIRS}
+    ${SDL2GFX_INCLUDE_DIRS}
     ${PNG_INCLUDE_DIR}
     ${PHYSFS_INCLUDE_DIR}
     ${CURL_INCLUDE_DIR}
@@ -115,19 +115,6 @@ IF (MINGW)
     SET(CMAKE_RC_COMPILE_OBJECT
       "<CMAKE_RC_COMPILER> <FLAGS> <DEFINES> -O coff -o <OBJECT> <SOURCE>")
 ENDIF()
-
-# Fix some stuff that gets not hidden by mainline modules
-MARK_AS_ADVANCED(PHYSFS_INCLUDE_DIR)
-MARK_AS_ADVANCED(PHYSFS_LIBRARY)
-MARK_AS_ADVANCED(SDLIMAGE_INCLUDE_DIR)
-MARK_AS_ADVANCED(SDLIMAGE_LIBRARY)
-MARK_AS_ADVANCED(SDLMAIN_LIBRARY)
-MARK_AS_ADVANCED(SDLMIXER_INCLUDE_DIR)
-MARK_AS_ADVANCED(SDLMIXER_LIBRARY)
-MARK_AS_ADVANCED(SDLNET_INCLUDE_DIR)
-MARK_AS_ADVANCED(SDLNET_LIBRARY)
-MARK_AS_ADVANCED(SDL_INCLUDE_DIR)
-MARK_AS_ADVANCED(SDL_LIBRARY)
 
 SET(SRCS
     gui/widgets/avatarlistbox.cpp
@@ -677,12 +664,12 @@ ENDIF(ENABLE_MANASERV)
 
 TARGET_LINK_LIBRARIES(mana
     ${INTERNAL_LIBRARIES}
-    ${SDLGFX_LIBRARIES}
-    ${SDL_LIBRARY}
-    ${SDLIMAGE_LIBRARY}
-    ${SDLMIXER_LIBRARY}
-    ${SDLNET_LIBRARY}
-    ${SDLTTF_LIBRARY}
+    ${SDL2_LIBRARIES}
+    ${SDL2GFX_LIBRARIES}
+    ${SDL2IMAGE_LIBRARIES}
+    ${SDL2MIXER_LIBRARIES}
+    ${SDL2NET_LIBRARIES}
+    ${SDL2TTF_LIBRARIES}
     ${PNG_LIBRARIES}
     ${PHYSFS_LIBRARY}
     ${CURL_LIBRARIES}

src/client.cpp
@@ -241,11 +241,6 @@ Client::Client(const Options &options):
     }
     atexit(SDL_Quit);
 
-    SDL_EnableUNICODE(1);
-    SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
-
-    SDL_WM_SetCaption(branding.getValue("appName", "Mana").c_str(), NULL);
-
     ResourceManager *resman = ResourceManager::getInstance();
 
     if (!resman->setWriteDir(mLocalDataDir))
@@ -253,8 +248,6 @@ Client::Client(const Options &options):
         logger->error(strprintf("%s couldn't be set as home directory! "
                                 "Exiting.", mLocalDataDir.c_str()));
     }
-
-    Image::SDLsetEnableAlphaCache(config.getValue("alphaCache", true));
 
 #if defined __APPLE__
     CFBundleRef mainBundle = CFBundleGetMainBundle();
@@ -300,6 +293,39 @@ Client::Client(const Options &options):
     // Add the local data directory to PhysicsFS search path
     resman->addToSearchPath(mLocalDataDir, false);
 
+    bool useOpenGL = !mOptions.noOpenGL && (config.getValue("opengl", 1) == 1);
+
+    // Set up the transparency option for low CPU when not using OpenGL.
+    if (!useOpenGL && (config.getValue("disableTransparency", 0) == 1))
+        Image::SDLdisableTransparency();
+
+#ifdef USE_OPENGL
+    // Setup image loading for the right image format
+    Image::setLoadAsOpenGL(useOpenGL);
+
+    // Create the graphics context
+    graphics = useOpenGL ? new OpenGLGraphics : new Graphics;
+#else
+    // Create the graphics context
+    graphics = new Graphics;
+#endif
+
+    const int width = config.getIntValue("screenwidth");
+    const int height = config.getIntValue("screenheight");
+    const bool fullscreen = config.getBoolValue("screen");
+
+    // Try to set the desired video mode
+    if (!graphics->setVideoMode(width, height, fullscreen))
+    {
+        logger->error(strprintf("Couldn't set %dx%d video mode: %s",
+            width, height, SDL_GetError()));
+    }
+
+    SDL_SetWindowTitle(graphics->getTarget(),
+                       branding.getValue("appName", "Mana").c_str());
+
+    Image::setRenderer(graphics->getRenderer());
+
     std::string iconFile = branding.getValue("appIcon", "icons/mana");
 #ifdef _WIN32
     iconFile += ".ico";
@@ -325,41 +351,9 @@ Client::Client(const Options &options):
     mIcon = IMG_Load(iconFile.c_str());
     if (mIcon)
     {
-        SDL_SetAlpha(mIcon, SDL_SRCALPHA, SDL_ALPHA_OPAQUE);
-        SDL_WM_SetIcon(mIcon, NULL);
+        SDL_SetWindowIcon(graphics->getTarget(), mIcon);
     }
 #endif
-
-    bool useOpenGL = !mOptions.noOpenGL && (config.getValue("opengl", 1) == 1);
-
-    // Set up the transparency option for low CPU when not using OpenGL.
-    if (!useOpenGL && (config.getValue("disableTransparency", 0) == 1))
-        Image::SDLdisableTransparency();
-
-#ifdef USE_OPENGL
-
-    // Setup image loading for the right image format
-    Image::setLoadAsOpenGL(useOpenGL);
-
-    // Create the graphics context
-    graphics = useOpenGL ? new OpenGLGraphics : new Graphics;
-#else
-    // Create the graphics context
-    graphics = new Graphics;
-#endif
-
-    const int width = config.getIntValue("screenwidth");
-    const int height = config.getIntValue("screenheight");
-    const int bpp = 0;
-    const bool fullscreen = config.getBoolValue("screen");
-    const bool hwaccel = config.getBoolValue("hwaccel");
-
-    // Try to set the desired video mode
-    if (!graphics->setVideoMode(width, height, bpp, fullscreen, hwaccel))
-    {
-        logger->error(strprintf("Couldn't set %dx%dx%d video mode: %s",
-            width, height, bpp, SDL_GetError()));
-    }
 
     // Initialize for drawing
     graphics->_beginDraw();
@@ -511,8 +505,12 @@ int Client::exec()
                     case SDL_KEYDOWN:
                         break;
 
-                    case SDL_VIDEORESIZE:
-                        handleVideoResize(event.resize.w, event.resize.h);
+                    case SDL_WINDOWEVENT:
+                        switch (event.window.event) {
+                            case SDL_WINDOWEVENT_RESIZED:
+                                handleVideoResize(event.window.data1, event.window.data2);
+                                break;
+                        }
                         break;
                 }
 
@@ -1374,27 +1372,17 @@ void Client::accountLogin(LoginData *loginData)
 
 void Client::handleVideoResize(int width, int height)
 {
-    // Keep a minimum size. This isn't adhered to by the actual window, but
-    // it keeps some window positions from getting messed up.
-    width = std::max(640, width);
-    height = std::max(480, height);
-
     if (graphics->getWidth() == width && graphics->getHeight() == height)
         return;
 
-    if (graphics->changeVideoMode(width,
-                                  height,
-                                  graphics->getBpp(),
-                                  false,
-                                  graphics->getHWAccel()))
-    {
-        videoResized(width, height);
+    graphics->videoResized(width, height);
 
-        // Since everything appears to have worked out, remember to store the
-        // new size in the configuration.
-        config.setValue("screenwidth", width);
-        config.setValue("screenheight", height);
-    }
+    videoResized(width, height);
+
+    // Since everything appears to have worked out, remember to store the
+    // new size in the configuration.
+    config.setValue("screenwidth", width);
+    config.setValue("screenheight", height);
 }
 
 void Client::videoResized(int width, int height)
@@ -1409,4 +1397,19 @@ void Client::videoResized(int width, int height)
 
     if (mGame)
         mGame->videoResized(width, height);
+}
+
+bool Client::isActive()
+{
+    return !(SDL_GetWindowFlags(graphics->getTarget()) & SDL_WINDOW_MINIMIZED);
+}
+
+bool Client::hasInputFocus()
+{
+    return SDL_GetWindowFlags(graphics->getTarget()) & SDL_WINDOW_INPUT_FOCUS;
+}
+
+bool Client::hasMouseFocus()
+{
+    return SDL_GetWindowFlags(graphics->getTarget()) & SDL_WINDOW_MOUSE_FOCUS;
 }

src/client.h
@@ -29,7 +29,7 @@
 #include <guichan/actionlistener.hpp>
 
 #include <SDL.h>
-#include <SDL_framerate.h>
+#include <SDL2_framerate.h>
 
 #include <string>
 
@@ -214,14 +214,9 @@ public:
      */
     void videoResized(int width, int height);
 
-    static bool isActive()
-    { return SDL_GetAppState() & SDL_APPACTIVE; }
-
-    static bool hasInputFocus()
-    { return SDL_GetAppState() & SDL_APPINPUTFOCUS; }
-
-    static bool hasMouseFocus()
-    { return SDL_GetAppState() & SDL_APPMOUSEFOCUS; }
+    static bool isActive();
+    static bool hasInputFocus();
+    static bool hasMouseFocus();
 
 private:
     void initRootDir();

src/compoundsprite.cpp
@@ -20,7 +20,6 @@
 
 #include "compoundsprite.h"
 
-#include "game.h"
 #include "graphics.h"
 #include "map.h"
 
@@ -223,6 +222,7 @@ int CompoundSprite::getDuration() const
     return duration;
 }
 
+#if 0
 static void updateValues(int &dimension, int &pos, int imgDimUL, int imgDimRD, int imgOffset)
 {
     // Handle going beyond the left/up
@@ -238,11 +238,18 @@ static void updateValues(int &dimension, int &pos, int imgDimUL, int imgDimRD, i
     if (temp > dimension)
         dimension = temp;
 }
-
-#include "localplayer.h"
+#endif
 
 void CompoundSprite::redraw() const
 {
+#if 1   // TODO_SDL2: Does it make sense to implement CompoundSprite?
+    mWidth = mSprites.at(0)->getWidth();
+    mHeight = mSprites.at(0)->getHeight();
+    mOffsetX = 0;
+    mOffsetY = 0;
+    mNeedsRedraw = false;
+#else
+
 #ifdef USE_OPENGL
     // TODO OpenGL support
     if (Image::getLoadAsOpenGL())
@@ -295,7 +302,7 @@ void CompoundSprite::redraw() const
     int amask = 0xff000000;
 #endif
 
-    SDL_Surface *surface = SDL_CreateRGBSurface(SDL_HWSURFACE, mWidth, mHeight,
+    SDL_Surface *surface = SDL_CreateRGBSurface(0, mWidth, mHeight,
                                             32, rmask, gmask, bmask, amask);
 
     if (!surface)
@@ -322,7 +329,7 @@ void CompoundSprite::redraw() const
 
     delete graphics;
 
-    SDL_Surface *surfaceA = SDL_CreateRGBSurface(SDL_HWSURFACE, mWidth, mHeight,
+    SDL_Surface *surfaceA = SDL_CreateRGBSurface(0, mWidth, mHeight,
                                             32, rmask, gmask, bmask, amask);
 
     SDL_SetAlpha(surface, 0, SDL_ALPHA_OPAQUE);
@@ -338,4 +345,5 @@ void CompoundSprite::redraw() const
     SDL_FreeSurface(surfaceA);
 
     mNeedsRedraw = false;
+#endif
 }

src/game.cpp
@@ -398,10 +398,10 @@ void Game::handleInput()
     {
         bool used = false;
 
-        if (event.type == SDL_VIDEORESIZE)
+        if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED)
         {
             // Let the client deal with this one (it'll pass down from there)
-            Client::instance()->handleVideoResize(event.resize.w, event.resize.h);
+            Client::instance()->handleVideoResize(event.window.data1, event.window.data2);
         }
         // Keyboard events (for discontinuous keys)
         else if (event.type == SDL_KEYDOWN)

commit 84ffd4a723fd2cfa690dc013b078ea660d1cce6b

Fixed crash when taking a screenshot
Wrong format was passed to SDL_RenderReadPixels. Issue introduced in
2c51c98625b225cecfb9628c30d62d4e30f7e3e1.

Closes #52

src/graphics.cpp
@@ -319,7 +319,11 @@ SDL_Surface *Graphics::getScreenshot()
 
     SDL_Surface *screenshot = SDL_CreateRGBSurface(0, mWidth,
             mHeight, 24, rmask, gmask, bmask, amask);
-    SDL_RenderReadPixels(mRenderer, nullptr, SDL_PIXELFORMAT_RGB888, screenshot->pixels, screenshot->pitch);
+
+    SDL_RenderReadPixels(mRenderer, nullptr,
+                         screenshot->format->format,
+                         screenshot->pixels,
+                         screenshot->pitch);
 
     return screenshot;
 }