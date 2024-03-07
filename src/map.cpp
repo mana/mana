mana/mana

commit 264be2108c51837fa92085f6c839e66aebbcfc9e

Added support for map/layer mask
A custom "Mask" property on a layer or a "foregroundXmask" property on a
map can now be used in combination with the SMSG_MAP_MASK to dynamically
disable certain map layers from the server.

Feature previously seen on ManaPlus and implemented for Mana client for
compatibility.

Also added a ResourceRef class for automating the Resource reference
counting.

Closes #44

src/map.cpp
@@ -255,61 +255,46 @@ void Map::initializeAmbientLayers()
 {
     ResourceManager *resman = ResourceManager::getInstance();
 
+    auto addAmbientLayer = [=](const std::string &name, std::list<AmbientLayer*> &list)
+    {
+        if (Image *img = resman->getImage(getProperty(name + "image")))
+        {
+            auto ambientLayer = new AmbientLayer(img);
+            ambientLayer->mParallax = getFloatProperty(name + "parallax");
+            ambientLayer->mSpeedX = getFloatProperty(name + "scrollX");
+            ambientLayer->mSpeedY = getFloatProperty(name + "scrollY");
+            ambientLayer->mMask = getIntProperty(name + "mask", 1);
+
+            list.push_back(ambientLayer);
+
+            // The AmbientLayer takes control over the image.
+            img->decRef();
+        }
+    };
+
     // search for "foreground*" or "overlay*" (old term) in map properties
     for (int i = 0; /* terminated by a break */; i++)
     {
-        std::string name;
         if (hasProperty("foreground" + toString(i) + "image"))
         {
-            name = "foreground" + toString(i);
+            addAmbientLayer("foreground" + toString(i), mForegrounds);
         }
         else if (hasProperty("overlay" + toString(i) + "image"))
         {
-            name = "overlay" + toString(i);
+            addAmbientLayer("overlay" + toString(i), mForegrounds);
         }
         else
         {
             break; // the FOR loop
         }
-
-        Image *img = resman->getImage(getProperty(name + "image"));
-        const float speedX = getFloatProperty(name + "scrollX");
-        const float speedY = getFloatProperty(name + "scrollY");
-        const float parallax = getFloatProperty(name + "parallax");
-        const bool keepRatio = getBoolProperty(name + "keepratio");
-
-        if (img)
-        {
-            mForegrounds.push_back(
-                    new AmbientLayer(img, parallax, speedX, speedY, keepRatio));
-
-            // The AmbientLayer takes control over the image.
-            img->decRef();
-        }
     }
-
 
     // search for "background*" in map properties
     for (int i = 0;
          hasProperty("background" + toString(i) + "image");
          i++)
     {
-        const std::string name = "background" + toString(i);
-
-        Image *img = resman->getImage(getProperty(name + "image"));
-        const float speedX = getFloatProperty(name + "scrollX");
-        const float speedY = getFloatProperty(name + "scrollY");
-        const float parallax = getFloatProperty(name + "parallax");
-        const bool keepRatio = getBoolProperty(name + "keepratio");
-
-        if (img)
-        {
-            mBackgrounds.push_back(
-                    new AmbientLayer(img, parallax, speedX, speedY, keepRatio));
-
-            // The AmbientLayer takes control over the image.
-            img->decRef();
-        }
+        addAmbientLayer("background" + toString(i), mBackgrounds);
     }
 }
 
@@ -387,6 +372,9 @@ void Map::draw(Graphics *graphics, int scrollX, int scrollY)
     {
         for (; layeri != mLayers.end() && !overFringe; ++layeri)
         {
+            if (((*layeri)->getMask() & mMask) == 0)
+                continue;
+
             if ((*layeri)->isFringeLayer() && (mDebugFlags & DEBUG_SPECIAL2))
                 overFringe = true;
 
@@ -503,10 +491,16 @@ void Map::updateAmbientLayers(float scrollX, float scrollY)
     std::list<AmbientLayer*>::iterator i;
     for (i = mBackgrounds.begin(); i != mBackgrounds.end(); i++)
     {
+        if (((*i)->mMask & mMask) == 0)
+            continue;
+
         (*i)->update(timePassed, dx, dy);
     }
     for (i = mForegrounds.begin(); i != mForegrounds.end(); i++)
     {
+        if (((*i)->mMask & mMask) == 0)
+            continue;
+
         (*i)->update(timePassed, dx, dy);
     }
     mLastScrollX = scrollX;
@@ -541,6 +535,9 @@ void Map::drawAmbientLayers(Graphics *graphics, LayerType type,
     for (auto i = layers->begin();
          i != layers->end(); i++)
     {
+        if (((*i)->mMask & mMask) == 0)
+            continue;
+
         (*i)->draw(graphics, graphics->getWidth(), graphics->getHeight());
 
         // Detail 1: only one overlay, higher: all overlays
@@ -1040,4 +1037,9 @@ TileAnimation *Map::getAnimationForGid(int gid) const
 {
     auto i = mTileAnimations.find(gid);
     return (i == mTileAnimations.end()) ? NULL : i->second;
+}
+
+void Map::setMask(int mask)
+{
+    mMask = mask;
 }

src/resources/ambientlayer.cpp
@@ -25,20 +25,12 @@
 #include "resources/image.h"
 #include "resources/resourcemanager.h"
 
-AmbientLayer::AmbientLayer(Image *img, float parallax,
-                               float speedX, float speedY, bool keepRatio):
-    mImage(img), mParallax(parallax),
-    mPosX(0), mPosY(0),
-    mSpeedX(speedX), mSpeedY(speedY),
-    mKeepRatio(keepRatio)
-{
-    mImage->incRef();
-}
+AmbientLayer::AmbientLayer(Image *img)
+    : mImage(img)
+{}
 
 AmbientLayer::~AmbientLayer()
-{
-    mImage->decRef();
-}
+{}
 
 void AmbientLayer::update(int timePassed, float dx, float dy)
 {

src/resources/ambientlayer.h
@@ -21,6 +21,8 @@
 #ifndef RESOURCES_AMBIENTOVERLAY_H
 #define RESOURCES_AMBIENTOVERLAY_H
 
+#include "resource.h"
+
 class Graphics;
 class Image;
 
@@ -31,29 +33,24 @@ class AmbientLayer
          * Constructor.
          *
          * @param img       the image this overlay displays
-         * @param parallax  scroll factor based on camera position
-         * @param speedX    scrolling speed in x-direction
-         * @param speedY    scrolling speed in y-direction
-         * @param keepRatio  rescale the image to keep
-         * the same ratio than in 800x600 resolution mode.
          */
-        AmbientLayer(Image *img, float parallax,
-                       float speedX, float speedY, bool keepRatio = false);
-
+        AmbientLayer(Image *img);
         ~AmbientLayer();
 
         void update(int timePassed, float dx, float dy);
 
         void draw(Graphics *graphics, int x, int y);
 
+        float mParallax = 0;      /**< Scroll factor based on camera position. */
+        float mSpeedX = 0;        /**< Scrolling speed in X direction. */
+        float mSpeedY = 0;        /**< Scrolling speed in Y direction. */
+        int mMask = 1;
+        bool mKeepRatio = false;  /**< Keep overlay ratio on every resolution like in 800x600 */
+
     private:
-        Image *mImage;
-        float mParallax;
-        float mPosX;              /**< Current layer X position. */
-        float mPosY;              /**< Current layer Y position. */
-        float mSpeedX;            /**< Scrolling speed in X direction. */
-        float mSpeedY;            /**< Scrolling speed in Y direction. */
-        bool mKeepRatio;          /**< Keep overlay ratio on every resolution */
+        ResourceRef<Image> mImage;
+        float mPosX = 0;          /**< Current layer X position. */
+        float mPosY = 0;          /**< Current layer Y position. */
 };
 
 #endif

src/resources/resource.h
@@ -69,4 +69,72 @@ class Resource
         unsigned mRefCount;  /**< Reference count. */
 };
 
+/**
+ * Automatically counting Resource reference.
+ */
+template<typename RESOURCE>
+class ResourceRef
+{
+public:
+    // Allow implicit construction from RESOURCE *
+    ResourceRef(RESOURCE *resource = nullptr)
+        : mResource(resource)
+    {
+        if (mResource)
+            mResource->incRef();
+    }
+
+    // Copy constructor
+    ResourceRef(const ResourceRef &other)
+        : mResource(other.mResource)
+    {
+        if (mResource)
+            mResource->incRef();
+    }
+
+    // Move constructor
+    ResourceRef(ResourceRef &&other)
+        : mResource(other.mResource)
+    {
+        other.mResource = nullptr;
+    }
+
+    // Destructor
+    ~ResourceRef()
+    {
+        if (mResource)
+            mResource->decRef();
+    }
+
+    // Assignment operator
+    ResourceRef &operator=(const ResourceRef &other)
+    {
+        if (this != &other)
+        {
+            if (mResource)
+                mResource->decRef();
+
+            mResource = other.mResource;
+
+            if (mResource)
+                mResource->incRef();
+        }
+        return *this;
+    }
+
+    // Allow dereferencing
+    RESOURCE *operator->() const
+    { return mResource; }
+
+    RESOURCE *get() const
+    { return mResource; }
+
+    // Allow implicit conversion to RESOURCE *
+    operator RESOURCE *() const
+    { return mResource; }
+
+private:
+    RESOURCE *mResource;
+};
+
 #endif

src/map.h
@@ -131,9 +131,13 @@ class MapLayer
 
         int getTileDrawWidth(int x1, int y1, int endX, int &width) const;
 
+        void setMask(int mask) { mMask = mask; }
+        int getMask() const { return mMask; }
+
     private:
         int mX, mY;
         int mWidth, mHeight;
+        int mMask = 1;
         bool mIsFringeLayer;    /**< Whether the actors are drawn. */
         Image **mTiles;
         Map *mMap;              /** The mother map pointer */
@@ -335,6 +339,8 @@ class Map : public Properties
          */
         TileAnimation *getAnimationForGid(int gid) const;
 
+        void setMask(int mask);
+
     protected:
         friend class Actor;
 
@@ -415,6 +421,7 @@ class Map : public Properties
 
         std::map<int, TileAnimation*> mTileAnimations;
 
+        int mMask = 1;
 };
 
 #endif

src/net/tmwa/network.cpp
@@ -40,7 +40,7 @@
 // indicator for a variable-length packet
 const uint16_t VAR = 1;
 
-uint16_t packet_lengths[0x220] = {
+uint16_t packet_lengths[] = {
    10,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
@@ -82,9 +82,12 @@ uint16_t packet_lengths[0x220] = {
   VAR,VAR, 20, 10, 32,  9, 34, 14,  2,  6, 48, 56,VAR,  4,  5, 10,
 // #0x0200
    26,  0,  0,  0, 18,  0,  0,  0,  0,  0,  0, 19, 10,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    2,VAR, 16,  0,  8,VAR,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  VAR,122,VAR,VAR,VAR,VAR, 10,  0,  0,  0,  0,  0,  0,  0,  0,  0,
 };
 
+static const int packet_lengths_size
+     = static_cast<int>(sizeof(packet_lengths) / sizeof(uint16_t));
 const unsigned int BUFFER_SIZE = 65536;
 
 namespace TmwAthena {
@@ -287,7 +290,7 @@ bool Network::messageReady()
     // TODO don't hard-code this single case
     if (msgId == SMSG_SERVER_VERSION_RESPONSE)
         len = 10;
-    else if (msgId < 0x220)
+    else if (msgId < packet_lengths_size)
         len = packet_lengths[msgId];
 
     if (len == VAR)

src/net/tmwa/network.h
@@ -41,6 +41,7 @@
  * the protocol accordingly.
  */
 #define CLIENT_PROTOCOL_VERSION      1
+// 10 -> 11: SMSG_MAP_MASK DONE
 
 namespace TmwAthena {
 

src/net/tmwa/playerhandler.cpp
@@ -152,6 +152,7 @@ PlayerHandler::PlayerHandler()
         SMSG_PLAYER_STAT_UPDATE_5,
         SMSG_PLAYER_STAT_UPDATE_6,
         SMSG_PLAYER_ARROW_MESSAGE,
+        SMSG_MAP_MASK,
         0
     };
     handledMessages = _messages;
@@ -510,6 +511,15 @@ void PlayerHandler::handleMessage(MessageIn &msg)
                         logger->log("0x013b: Unhandled message %i", type);
                         break;
                 }
+            }
+            break;
+
+        case SMSG_MAP_MASK:
+            {
+                const int mask = msg.readInt32();
+                msg.readInt32();  // unused
+                if (Map *map = Game::instance()->getCurrentMap())
+                    map->setMask(mask);
             }
             break;
     }

commit 98e0c2435bd69d6dd78fb0f84389a81b56278958

Fixed ambient layers "keepratio" option
I had broken this in 264be2108c51837fa92085f6c839e66aebbcfc9e by no
longer reading out the property.

Made some further simplifications and two more fixes:

* When the original image's size was not 800x600, it wouldn't get scaled
  as expected due to the integer division being performed before the
  multiplication.

* I had changed the default resolution to 1280x720, while mKeepRatio
  expects to scale the original image as if it was displayed on an
  800x600 screen. So now it's hardcoded on 800x600 rather than using
  the defaultScreenWidth/Height variables.

* Removed the confusing x/y parameters from AmbientLayer::draw, which
  were actually set to the graphics width/height.

* Changed SDLGraphics::drawRescaledImagePattern to not repeatedly
  set the source origin.

src/map.cpp
@@ -252,6 +252,7 @@ void Map::initializeAmbientLayers()
             ambientLayer->mSpeedX = getFloatProperty(name + "scrollX");
             ambientLayer->mSpeedY = getFloatProperty(name + "scrollY");
             ambientLayer->mMask = getIntProperty(name + "mask", 1);
+            ambientLayer->mKeepRatio = getBoolProperty(name + "keepratio");
 
             list.push_back(ambientLayer);
 
@@ -501,7 +502,7 @@ void Map::drawAmbientLayers(Graphics *graphics, LayerType type,
         if ((layer->mMask & mMask) == 0)
             continue;
 
-        layer->draw(graphics, graphics->getWidth(), graphics->getHeight());
+        layer->draw(graphics);
 
         // Detail 1: only one overlay, higher: all overlays
         if (detail == 1)

src/resources/ambientlayer.cpp
@@ -21,7 +21,6 @@
 #include "resources/ambientlayer.h"
 
 #include "graphics.h"
-#include "video.h"
 
 #include "resources/image.h"
 #include "resources/resourcemanager.h"
@@ -30,8 +29,7 @@ AmbientLayer::AmbientLayer(Image *img)
     : mImage(img)
 {}
 
-AmbientLayer::~AmbientLayer()
-{}
+AmbientLayer::~AmbientLayer() = default;
 
 void AmbientLayer::update(int timePassed, float dx, float dy)
 {
@@ -58,14 +56,27 @@ void AmbientLayer::update(int timePassed, float dx, float dy)
         mPosY += imgH;
 }
 
-void AmbientLayer::draw(Graphics *graphics, int x, int y)
+void AmbientLayer::draw(Graphics *graphics)
 {
+    const auto screenWidth = graphics->getWidth();
+    const auto screenHeight = graphics->getHeight();
+    const auto x = static_cast<int>(mPosX);
+    const auto y = static_cast<int>(mPosY);
+
     if (!mKeepRatio)
+    {
         graphics->drawImagePattern(mImage,
-            (int) -mPosX, (int) -mPosY, x + (int) mPosX, y + (int) mPosY);
+                                   -x, -y,
+                                   screenWidth + x,
+                                   screenHeight + y);
+    }
     else
+    {
         graphics->drawRescaledImagePattern(mImage,
-              (int) -mPosX, (int) -mPosY, x + (int) mPosX, y + (int) mPosY,
-         (int) mImage->getWidth() / defaultScreenWidth * graphics->getWidth(),
-     (int) mImage->getHeight() / defaultScreenHeight * graphics->getHeight());
+                                           -x, -y,
+                                           screenWidth + x,
+                                           screenHeight + y,
+                                           mImage->getWidth() * screenWidth / 800,
+                                           mImage->getHeight() * screenHeight / 600);
+    }
 }

src/resources/ambientlayer.h
@@ -39,7 +39,7 @@ class AmbientLayer
 
         void update(int timePassed, float dx, float dy);
 
-        void draw(Graphics *graphics, int x, int y);
+        void draw(Graphics *graphics);
 
         float mParallax = 0;      /**< Scroll factor based on camera position. */
         float mSpeedX = 0;        /**< Scrolling speed in X direction. */

src/resources/resource.h
@@ -59,7 +59,7 @@ class Resource
         { return mIdPath; }
 
     protected:
-        virtual ~Resource() {}
+        virtual ~Resource() = default;
 
     private:
         std::string mIdPath;    /**< Path identifying this resource. */

src/sdlgraphics.cpp
@@ -118,23 +118,23 @@ void SDLGraphics::drawRescaledImagePattern(Image *image,
     if (scaledHeight <= 0 || scaledWidth <= 0)
         return;
 
+    SDL_Rect srcRect;
+    srcRect.x = image->mBounds.x;
+    srcRect.y = image->mBounds.y;
+
     for (int py = 0; py < h; py += scaledHeight)    // Y position on pattern plane
     {
         int dh = (py + scaledHeight >= h) ? h - py : scaledHeight;
-        int srcY = image->mBounds.y;
         int dstY = y + py + mClipStack.top().yOffset;
 
         for (int px = 0; px < w; px += scaledWidth) // X position on pattern plane
         {
             int dw = (px + scaledWidth >= w) ? w - px : scaledWidth;
-            int srcX = image->mBounds.x;
             int dstX = x + px + mClipStack.top().xOffset;
 
             SDL_Rect dstRect;
-            SDL_Rect srcRect;
             dstRect.x = dstX; dstRect.y = dstY;
             dstRect.w = dw;   dstRect.h = dh;
-            srcRect.x = srcX; srcRect.y = srcY;
             srcRect.w = dw;   srcRect.h = dh;
 
             if (SDL_RenderCopy(mRenderer, image->mTexture, &srcRect, &dstRect))