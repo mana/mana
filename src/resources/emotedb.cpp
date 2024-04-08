mana/mana

commit 14dc8218eeebd1583e6bd3c49013b3e501f47228

Use ResourceRef<Image> in more places
Automatic reference counting of images is now used by Item, Icon,
AnimatedSprite, ImageSprite, ParticleEmitter, Minimap, Desktop and
Emote.

Since ResourceManager::get automatically adds a reference, it needs to
be explicitly subtracted when the resource is managed by ResourceRef.
This is taken care of by the new ResourceManager::getImageRef.

Also removed the apprently unused and duplicate "mDrawImage" from Item
(which also didn't get decRef called on it).

Fixes cleanup of emote ImageSet and ImageSprite instances, as well as
particle images.

src/resources/emotedb.cpp
@@ -44,8 +44,8 @@ void EmoteDB::init()
 
     mUnknown.name = "unknown";
     mUnknown.effectId = -1;
-    mUnknown.sprite = new ImageSprite(
-        ResourceManager::getInstance()->getImage("graphics/sprites/error.png"));
+    mUnknown.sprite = std::make_unique<ImageSprite>(
+        ResourceManager::getInstance()->getImageRef("graphics/sprites/error.png"));
 }
 
 void EmoteDB::readEmoteNode(xmlNodePtr node, const std::string &filename)
@@ -57,16 +57,16 @@ void EmoteDB::readEmoteNode(xmlNodePtr node, const std::string &filename)
         return;
     }
 
-    Emote currentEmote;
+    Emote emote;
 
-    currentEmote.id = id;
-    currentEmote.name = XML::getProperty(node, "name", "unknown");
-    currentEmote.effectId = XML::getProperty(node, "effectid", -1);
+    emote.id = id;
+    emote.name = XML::getProperty(node, "name", "unknown");
+    emote.effectId = XML::getProperty(node, "effectid", -1);
 
-    if (currentEmote.effectId == -1)
+    if (emote.effectId == -1)
     {
         logger->log("Emote Database: Warning: Emote %s has no attached effect in %s!",
-                    currentEmote.name.c_str(), filename.c_str());
+                    emote.name.c_str(), filename.c_str());
         return;
     }
 
@@ -77,25 +77,26 @@ void EmoteDB::readEmoteNode(xmlNodePtr node, const std::string &filename)
     if (imageName.empty() || width <= 0 || height <= 0)
     {
         logger->log("Emote Database: Warning: Emote %s has bad imageset values in %s",
-                    currentEmote.name.c_str(), filename.c_str());
+                    emote.name.c_str(), filename.c_str());
         return;
     }
 
-    ImageSet *is = ResourceManager::getInstance()->getImageSet(imageName,
-                                                               width,
-                                                               height);
-    if (!is || is->size() == 0)
+    emote.is = ResourceManager::getInstance()->getImageSet(imageName,
+                                                           width,
+                                                           height);
+    emote.is->decRef(); // clear automatic reference
+
+    if (!emote.is || emote.is->size() == 0)
     {
         logger->log("Emote Database: Error loading imageset for emote %s in %s",
-                    currentEmote.name.c_str(), filename.c_str());
-        delete is;
+                    emote.name.c_str(), filename.c_str());
         return;
     }
 
     // For now we just use the first image in the animation
-    currentEmote.sprite = new ImageSprite(is->get(0));
+    emote.sprite = std::make_unique<ImageSprite>(emote.is->get(0));
 
-    mEmotes.push_back(std::move(currentEmote));
+    mEmotes.push_back(std::move(emote));
 }
 
 void EmoteDB::checkStatus()
@@ -105,7 +106,9 @@ void EmoteDB::checkStatus()
 
 void EmoteDB::unload()
 {
-    // todo: don't we need to delete the sprites?
+    for (auto &emote : mEmotes)
+        emote.sprite->releaseImageRef();
+
     mEmotes.clear();
     mLoaded = false;
 }

src/animatedsprite.cpp
@@ -35,9 +35,6 @@ AnimatedSprite::AnimatedSprite(SpriteDef *sprite):
 {
     assert(mSprite);
 
-    // Take possession of the sprite
-    mSprite->incRef();
-
     // Play the stand animation by default
     play(SpriteAction::STAND);
 }
@@ -53,10 +50,7 @@ AnimatedSprite *AnimatedSprite::load(const std::string &filename, int variant)
     return as;
 }
 
-AnimatedSprite::~AnimatedSprite()
-{
-    mSprite->decRef();
-}
+AnimatedSprite::~AnimatedSprite() = default;
 
 bool AnimatedSprite::reset()
 {

src/animatedsprite.h
@@ -87,7 +87,7 @@ class AnimatedSprite final : public Sprite
         int mFrameIndex = 0;                /**< The index of the current frame. */
         int mFrameTime = 0;                 /**< The time since start of frame. */
 
-        SpriteDef *mSprite;                 /**< The sprite definition. */
+        ResourceRef<SpriteDef> mSprite;     /**< The sprite definition. */
         Action *mAction = nullptr;          /**< The currently active action. */
         Animation *mAnimation = nullptr;    /**< The currently active animation. */
         Frame *mFrame = nullptr;            /**< The currently active frame. */

src/animationparticle.h
@@ -27,8 +27,6 @@
 
 #include <libxml/tree.h>
 
-#include <memory>
-
 class Map;
 
 class AnimationParticle : public ImageParticle

src/gui/itempopup.cpp
@@ -102,7 +102,7 @@ ItemPopup::ItemPopup():
     mItemWeight = new TextBox;
     mItemWeight->setEditable(false);
 
-    mIcon = new Icon(nullptr);
+    mIcon = new Icon;
 
     add(mItemName);
     add(mItemDesc);
@@ -113,15 +113,7 @@ ItemPopup::ItemPopup():
     addMouseListener(this);
 }
 
-ItemPopup::~ItemPopup()
-{
-    if (mIcon)
-    {
-        Image *image = mIcon->getImage();
-        if (image)
-            image->decRef();
-    }
-}
+ItemPopup::~ItemPopup() = default;
 
 void ItemPopup::setEquipmentText(const std::string& text)
 {
@@ -158,16 +150,11 @@ void ItemPopup::setItem(const ItemInfo &item, bool showImage)
 
     int space = 0;
 
-    Image *oldImage = mIcon->getImage();
-    if (oldImage)
-        oldImage->decRef();
-
     if (showImage)
     {
         ResourceManager *resman = ResourceManager::getInstance();
-        Image *image = resman->getImage(
-                                  paths.getStringValue("itemIcons")
-                                  + item.getDisplay().image);
+        auto image = resman->getImageRef(paths.getStringValue("itemIcons") +
+                                         item.getDisplay().image);
 
         mIcon->setImage(image);
         if (image)

src/gui/minimap.cpp
@@ -65,16 +65,12 @@ Minimap::Minimap():
 Minimap::~Minimap()
 {
     config.setValue(getWindowName() + "Show", mShow);
-
-    if (mMapImage)
-        mMapImage->decRef();
 }
 
 void Minimap::setMap(Map *map)
 {
     // Set the title for the Minimap
     std::string caption;
-    std::string minimapName;
 
     if (map)
         caption = map->getName();
@@ -85,11 +81,7 @@ void Minimap::setMap(Map *map)
     minimap->setCaption(caption);
 
     // Adapt the image
-    if (mMapImage)
-    {
-        mMapImage->decRef();
-        mMapImage = nullptr;
-    }
+    mMapImage = nullptr;
 
     if (map)
     {
@@ -98,13 +90,13 @@ void Minimap::setMap(Map *map)
             "graphics/minimaps/" + map->getFilename() + ".png";
         ResourceManager *resman = ResourceManager::getInstance();
 
-        minimapName = map->getProperty("minimap");
+        std::string minimapName = map->getProperty("minimap");
 
         if (minimapName.empty() && resman->exists(tempname))
             minimapName = tempname;
 
         if (!minimapName.empty())
-            mMapImage = resman->getImage(minimapName);
+            mMapImage = resman->getImageRef(minimapName);
     }
 
     if (mMapImage)

src/gui/minimap.h
@@ -24,6 +24,8 @@
 
 #include "gui/widgets/window.h"
 
+#include "resources/resource.h"
+
 class Image;
 class Map;
 
@@ -59,7 +61,7 @@ class Minimap : public Window
 
     private:
         Map *mMap = nullptr;
-        Image *mMapImage = nullptr;
+        ResourceRef<Image> mMapImage;
         float mWidthProportion = 0.5;
         float mHeightProportion = 0.5;
         static bool mShow;

src/gui/widgets/desktop.cpp
@@ -52,11 +52,7 @@ Desktop::Desktop()
     add(mVersionLabel, 25, 2);
 }
 
-Desktop::~Desktop()
-{
-    if (mWallpaper)
-        mWallpaper->decRef();
-}
+Desktop::~Desktop() = default;
 
 void Desktop::reloadWallpaper()
 {
@@ -109,14 +105,11 @@ void Desktop::setBestFittingWallpaper()
         return;
 
     ResourceManager *resman = ResourceManager::getInstance();
-    Image *wallpaper = resman->getImage(wallpaperName);
+    auto wallpaper = resman->getImageRef(wallpaperName);
 
     if (wallpaper)
     {
-        if (mWallpaper)
-            mWallpaper->decRef(Resource::DeleteImmediately);
-
-        mWallpaper = wallpaper;
+        mWallpaper = std::move(wallpaper);
     }
     else
     {

commit 20ec47a6504f5ebed9a4986e3d004f3445e3f987

Fixed assertion failure on exit
Regression introduced in 14dc8218eeebd1583e6bd3c49013b3e501f47228.

src/resources/emotedb.cpp
@@ -110,6 +110,10 @@ void EmoteDB::unload()
         emote.sprite->releaseImageRef();
 
     mEmotes.clear();
+
+    if (mUnknown.sprite)
+        mUnknown.sprite->releaseImageRef();
+
     mLoaded = false;
 }
 