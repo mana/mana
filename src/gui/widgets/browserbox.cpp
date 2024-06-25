ManaPlus/ManaPlus

Show item links with empty item name.

src/gui/widgets/browserbox.cpp
@@ -36,7 +36,10 @@
 
 #include "resources/image.h"
 #include "resources/imageset.h"
+#include "resources/iteminfo.h"
 #include "resources/resourcemanager.h"
+
+#include "resources/db/itemdb.h"
 
 #include "utils/stringutils.h"
 #include "utils/timer.h"
@@ -243,6 +246,14 @@ void BrowserBox::addRow(const std::string &row, const bool atTop)
             bLink.caption = tmp.substr(idx2 + 1, idx3 - (idx2 + 1));
             bLink.y1 = sz * font->getHeight();
             bLink.y2 = bLink.y1 + font->getHeight();
+            if (bLink.caption.empty())
+            {
+                const int id = atoi(bLink.link.c_str());
+                if (id)
+                    bLink.caption = ItemDB::get(id).getName();
+                else
+                    bLink.caption = bLink.link;
+            }
 
             newRow.append(tmp.substr(0, idx1));
 

mana/mana

(cherry picked from commit 0192d44d46dcf948ab26371862753f4baf5aae4e)

src/gui/widgets/browserbox.cpp
@@ -28,6 +28,8 @@
 #include "gui/truetypefont.h"
 #include "gui/widgets/linkhandler.h"
 
+#include "resources/itemdb.h"
+#include "resources/iteminfo.h"
 #include "resources/theme.h"
 
 #include <guichan/graphics.hpp>
@@ -95,6 +97,15 @@ void BrowserBox::addRow(const std::string &row)
             BrowserLink &link = newRow.links.emplace_back();
             link.link = tmp.substr(idx1 + 2, idx2 - (idx1 + 2));
             link.caption = tmp.substr(idx2 + 1, idx3 - (idx2 + 1));
+
+            if (link.caption.empty())
+            {
+                const int id = atoi(link.link.c_str());
+                if (id)
+                    link.caption = itemDb->get(id).getName();
+                else
+                    link.caption = link.link;
+            }
 
             newRow.text += tmp.substr(0, idx1);
             newRow.text += "##<" + link.caption;