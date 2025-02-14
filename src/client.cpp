mana/mana

commit f405849b49614254f42eb3ee6147434458978623

Made the client able to remove the port from update urls.
Resolves: Mana-Mantis #381.

Reviewed-by: Ablu.

src/client.cpp
@@ -1267,46 +1267,30 @@ void Client::initConfiguration()
  */
 void Client::initUpdatesDir()
 {
-    std::stringstream updates;
-
     // If updatesHost is currently empty, fill it from config file
     if (mUpdateHost.empty())
-    {
         mUpdateHost = config.getStringValue("updatehost");
-    }
 
-    // Don't go out of range int he next check
-    if (mUpdateHost.length() < 2)
+    // Exit on empty update host.
+    if (mUpdateHost.empty())
         return;
 
-    // Remove any trailing slash at the end of the update host
-    if (!mUpdateHost.empty() && mUpdateHost.at(mUpdateHost.size() - 1) == '/')
-        mUpdateHost.resize(mUpdateHost.size() - 1);
+    logger->log("Setting update host: %s", mUpdateHost.c_str());
 
-    // Parse out any "http://" or "ftp://", and set the updates directory
-    size_t pos;
-    pos = mUpdateHost.find("://");
-    if (pos != mUpdateHost.npos)
+    std::string updateHost = getHostNameFromURL(mUpdateHost);
+
+    // Exit on a wrong update host.
+    if (updateHost.length() < 2)
     {
-        if (pos + 3 < mUpdateHost.length() && !mUpdateHost.empty())
-        {
-            updates << "updates/" << mUpdateHost.substr(pos + 3);
-            mUpdatesDir = updates.str();
-        }
-        else
-        {
-            logger->log("Error: Invalid update host: %s", mUpdateHost.c_str());
-            errorMessage = strprintf(_("Invalid update host: %s"),
-                                     mUpdateHost.c_str());
-            mState = STATE_ERROR;
-        }
+        // Show the original updateHostname in the error message.
+        errorMessage = strprintf(_("Invalid update host: %s"),
+                                 mUpdateHost.c_str());
+        mState = STATE_ERROR;
+        return;
     }
-    else
-    {
-        logger->log("Warning: no protocol was specified for the update host");
-        updates << "updates/" << mUpdateHost;
-        mUpdatesDir = updates.str();
-    }
+
+    mUpdateHost = updateHost;
+    mUpdatesDir = "updates/" + mUpdateHost;
 
     ResourceManager *resman = ResourceManager::getInstance();
 
@@ -1330,13 +1314,17 @@ void Client::initUpdatesDir()
             {
                 logger->log("Error: %s can't be made, but doesn't exist!",
                             newDir.c_str());
-                errorMessage = _("Error creating updates directory!");
+                errorMessage =
+                    strprintf(_("Error creating updates directory!\n(%s)"),
+                                newDir.c_str());
                 mState = STATE_ERROR;
             }
 #else
             logger->log("Error: %s/%s can't be made, but doesn't exist!",
                         mLocalDataDir.c_str(), mUpdatesDir.c_str());
-            errorMessage = _("Error creating updates directory!");
+            errorMessage =
+                strprintf(_("Error creating updates directory!\n(%s/%s)"),
+                            mLocalDataDir.c_str(), mUpdatesDir.c_str());
             mState = STATE_ERROR;
 #endif
         }

src/utils/stringutils.cpp
@@ -21,6 +21,8 @@
 
 #include "utils/stringutils.h"
 
+#include "log.h"
+
 #include <string.h>
 #include <algorithm>
 #include <cstdarg>
@@ -227,5 +229,50 @@ std::string autocomplete(std::vector<std::string> &candidates,
 std::string normalize(const std::string &name)
 {
     std::string normalized = name;
-    return toLower(trim(normalized));;
+    return toLower(trim(normalized));
+}
+
+std::string removeTrailingSymbol(const std::string& s, const char c)
+{
+    // Remove the trailing symblol at the end of the string
+    if (!s.empty() && s.at(s.size() - 1) == c)
+        return s.substr(0, s.size() - 1);
+    return std::string(s);
+}
+
+std::string getHostNameFromURL(const std::string& url)
+{
+    std::string myHostName;
+
+    // Don't go out of range in the next check
+    if (url.length() < 2)
+        return myHostName;
+
+    // Remove any trailing slash at the end of the update host
+    myHostName = removeTrailingSymbol(url, '/');
+
+    // Parse out any "http://", "ftp://", ect...
+    size_t pos = myHostName.find("://");
+    if (pos == myHostName.npos)
+    {
+        logger->log("Warning: no protocol was specified for the url: %s",
+                    url.c_str());
+    }
+
+    if (myHostName.empty() || pos + 3 >= myHostName.length())
+    {
+        logger->log("Error: Invalid url: %s", url.c_str());
+        return myHostName;
+    }
+    myHostName = myHostName.substr(pos + 3);
+
+    // Remove possible trailing port (i.e.: localhost:8000 -> localhost)
+    pos = myHostName.find(":");
+    if (pos != myHostName.npos)
+        myHostName = myHostName.substr(0, pos);
+
+    // remove possible other junk
+    removeBadChars(myHostName);
+
+    return myHostName;
 }

src/utils/stringutils.h
@@ -149,4 +149,15 @@ std::string autocomplete(std::vector<std::string> &candidates,
  */
 std::string normalize(const std::string &name);
 
+/**
+ * Remove a potential trailing symbol from a string.
+ */
+std::string removeTrailingSymbol(const std::string& s, const char c);
+
+/**
+ * Gets the hostname out of the given URL.
+ * i.e.: http://www.manasource.org:9601 -> www.manasource.org
+ */
+std::string getHostNameFromURL(const std::string& url);
+
 #endif // UTILS_STRINGUTILS_H

commit 9424885c8e8d78230aa8de1c9083b3493a615222

Fixed handling of update URLs which mention a port
This appears to have been a regression in
f405849b49614254f42eb3ee6147434458978623, which for some reason erased
the port (and any trailing parts) from not just the update directory but
also from the update URL.

Unfortunately we can't access Mana-Mantis #381 at the moment, but
presumably the port was removed due to the colon being a problematic
character. Instead, now the colon (and other special characters) are
replaced by _ when determining the update directory.

Closes https://git.themanaworld.org/mana/mana/-/issues/80

src/client.cpp
@@ -654,22 +654,17 @@ int Client::exec()
                     break;
 
                 case STATE_UPDATE:
-                    // Determine which source to use for the update host
-                    if (!mOptions.updateHost.empty())
-                        mUpdateHost = mOptions.updateHost;
-                    else
-                        mUpdateHost = loginData.updateHost;
-                    initUpdatesDir();
+                    logger->log("State: UPDATE");
 
                     if (mOptions.skipUpdate)
                     {
                         mState = STATE_LOAD_DATA;
                     }
-                    else
+                    else if (initUpdatesDir())
                     {
-                        logger->log("State: UPDATE");
                         mCurrentDialog = new UpdaterWindow(mUpdateHost,
-                                mLocalDataDir + "/" + mUpdatesDir,mOptions.dataPath.empty());
+                                                           mLocalDataDir + "/" + mUpdatesDir,
+                                                           mOptions.dataPath.empty());
                     }
                     break;
 
@@ -1104,39 +1099,39 @@ void Client::initConfiguration()
  * Parse the update host and determine the updates directory
  * Then verify that the directory exists (creating if needed).
  */
-void Client::initUpdatesDir()
+bool Client::initUpdatesDir()
 {
-    // If updatesHost is currently empty, fill it from config file
-    if (mUpdateHost.empty())
+    // Determine which source to use for the update host
+    if (!mOptions.updateHost.empty())
+        mUpdateHost = mOptions.updateHost;
+    else if (!loginData.updateHost.empty())
+        mUpdateHost = loginData.updateHost;
+    else
         mUpdateHost = config.updatehost;
 
-    // Exit on empty update host.
+    // Remove any trailing slashes at the end of the URL
+    while (!mUpdateHost.empty() && mUpdateHost.back() == '/')
+        mUpdateHost.pop_back();
+
     if (mUpdateHost.empty())
-        return;
-
-    logger->log("Setting update host: %s", mUpdateHost.c_str());
-
-    std::string updateHost = getHostNameFromURL(mUpdateHost);
-
-    // Exit on a wrong update host.
-    if (updateHost.length() < 2)
     {
-        // Show the original updateHostname in the error message.
-        errorMessage = strprintf(_("Invalid update host: %s"),
-                                 mUpdateHost.c_str());
-        mState = STATE_ERROR;
-        return;
+        logger->log("No update host provided");
+        mUpdatesDir.clear();
+        mState = STATE_LOAD_DATA;
+        return false;
     }
 
-    mUpdateHost = updateHost;
-    mUpdatesDir = "updates/" + mUpdateHost;
+    mUpdatesDir = "updates/" + getDirectoryFromURL(mUpdateHost);
+
+    logger->log("Update host: %s", mUpdateHost.c_str());
+    logger->log("Updates dir: %s", mUpdatesDir.c_str());
 
     ResourceManager *resman = ResourceManager::getInstance();
 
     // Verify that the updates directory exists. Create if necessary.
-    if (!resman->isDirectory("/" + mUpdatesDir))
+    if (!resman->isDirectory(mUpdatesDir))
     {
-        if (!resman->mkdir("/" + mUpdatesDir))
+        if (!resman->mkdir(mUpdatesDir))
         {
 #if defined _WIN32
             std::string newDir = mLocalDataDir + "\\" + mUpdatesDir;
@@ -1165,9 +1160,12 @@ void Client::initUpdatesDir()
                 strprintf(_("Error creating updates directory!\n(%s/%s)"),
                             mLocalDataDir.c_str(), mUpdatesDir.c_str());
             mState = STATE_ERROR;
+            return false;
 #endif
         }
     }
+
+    return true;
 }
 
 void Client::initScreenshotDir()

src/client.h
@@ -202,7 +202,7 @@ private:
     void initRootDir();
     void initHomeDir();
     void initConfiguration();
-    void initUpdatesDir();
+    bool initUpdatesDir();
     void initScreenshotDir();
 
     void accountLogin(LoginData *loginData);

src/gui/updaterwindow.h
@@ -64,7 +64,7 @@ class UpdaterWindow : public Window, public gcn::ActionListener,
      * @param updateHost Host where to get the updated files.
      * @param updatesDir Directory where to store updates (should be absolute
      *                   and already created).
-     * @param applyUpdates If true, the update window will pass the updates to teh
+     * @param applyUpdates If true, the update window will pass the updates to the
      *                     resource manager
      */
     UpdaterWindow(const std::string &updateHost,

src/net/tmwa/loginhandler.cpp
@@ -103,17 +103,15 @@ void LoginHandler::handleMessage(MessageIn &msg)
         }
             break;
 
-        case SMSG_UPDATE_HOST:
-             int len;
-
-             len = msg.readInt16() - 4;
+        case SMSG_UPDATE_HOST: {
+             int len = msg.readInt16() - 4;
              mUpdateHost = msg.readString(len);
              loginData.updateHost = mUpdateHost;
 
              logger->log("Received update host \"%s\" from login server.",
                      mUpdateHost.c_str());
              break;
-
+        }
         case SMSG_LOGIN_DATA:
             // Skip the length word
             msg.skip(2);

src/utils/stringutils.cpp
@@ -101,16 +101,15 @@ std::string strprintf(char const *format, ...)
     return res;
 }
 
-std::string &removeBadChars(std::string &str)
+std::string &replaceCharacters(std::string &str,
+                               std::string_view chars,
+                               char replacement)
 {
-    std::string::size_type pos;
-    do
+    for (auto &c : str)
     {
-        pos = str.find_first_of("@#[]");
-        if (pos != std::string::npos)
-            str.erase(pos, 1);
-    } while (pos != std::string::npos);
-
+        if (chars.find(c) != std::string::npos)
+            c = replacement;
+    }
     return str;
 }
 
@@ -220,49 +219,27 @@ std::string normalize(const std::string &name)
     return toLower(trim(normalized));
 }
 
-std::string removeTrailingSymbol(const std::string &s, const char c)
+std::string getDirectoryFromURL(const std::string &url)
 {
-    // Remove the trailing symblol at the end of the string
-    if (!s.empty() && s.at(s.size() - 1) == c)
-        return s.substr(0, s.size() - 1);
-    return std::string(s);
-}
+    std::string directory = url;
 
-std::string getHostNameFromURL(const std::string &url)
-{
-    std::string myHostName;
-
-    // Don't go out of range in the next check
-    if (url.length() < 2)
-        return myHostName;
-
-    // Remove any trailing slash at the end of the update host
-    myHostName = removeTrailingSymbol(url, '/');
-
-    // Parse out any "http://", "ftp://", ect...
-    size_t pos = myHostName.find("://");
-    if (pos == std::string::npos)
-    {
-        logger->log("Warning: no protocol was specified for the url: %s",
-                    url.c_str());
-    }
-
-    if (myHostName.empty() || pos + 3 >= myHostName.length())
-    {
-        logger->log("Error: Invalid url: %s", url.c_str());
-        return myHostName;
-    }
-    myHostName = myHostName.substr(pos + 3);
-
-    // Remove possible trailing port (i.e.: localhost:8000 -> localhost)
-    pos = myHostName.find(":");
+    // Parse out any "http://", "ftp://", etc...
+    size_t pos = directory.find("://");
     if (pos != std::string::npos)
-        myHostName = myHostName.substr(0, pos);
+        directory.erase(0, pos + 3);
 
-    // remove possible other junk
-    removeBadChars(myHostName);
+    // Replace characters which are not valid or difficult in file system paths
+    replaceCharacters(directory, ":*?\"<>| ", '_');
 
-    return myHostName;
+    // Replace ".." (double dots) with "_" to avoid directory traversal.
+    pos = directory.find("..");
+    while (pos != std::string::npos)
+    {
+        directory.replace(pos, 2, "_");
+        pos = directory.find("..");
+    }
+
+    return directory;
 }
 
 std::string join(const std::vector<std::string> &strings, const char *separator)

src/utils/stringutils.h
@@ -95,12 +95,16 @@ std::string strprintf(char const *, ...)
 ;
 
 /**
- * Removes bad characters from a string
+ * Replaces a set of characters with another character.
  *
  * @param str the string to remove the bad chars from
+ * @param chars the bad characters to remove
+ * @param replacement the character to replace the bad chars with
  * @return a reference to the string without bad chars
  */
-std::string &removeBadChars(std::string &str);
+std::string &replaceCharacters(std::string &str,
+                               std::string_view chars,
+                               char replacement = '_');
 
 /**
  * Removes colors from a string
@@ -210,15 +214,12 @@ std::string autocomplete(const std::vector<std::string> &candidates,
 std::string normalize(const std::string &name);
 
 /**
- * Remove a potential trailing symbol from a string.
+ * Derives a directory from the given URL, stripping the schema and replacing
+ * certain invalid characters.
+ *
+ * i.e.: http://www.manasource.org:9601/updates/ -> www.manasource.org_9601/updates/
  */
-std::string removeTrailingSymbol(const std::string &s, const char c);
-
-/**
- * Gets the hostname out of the given URL.
- * i.e.: http://www.manasource.org:9601 -> www.manasource.org
- */
-std::string getHostNameFromURL(const std::string &url);
+std::string getDirectoryFromURL(const std::string &url);
 
 /**
  * Joins a vector of strings into one string, separated by the given