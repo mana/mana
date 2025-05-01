mana/mana

commit 79e4325192f3260ed4ded264e43da8429650bf72

Removed needless case-insensitive string comparisons
These are not necessary since we can instead make sure the referenced
values match case, like we do for everything else.

This affects server types in the server list and colors referenced in
theme files. The server version was also compared case-insensitively
for some reason.

src/gui/serverdialog.cpp
@@ -455,7 +455,7 @@ void ServerDialog::loadServer(XML::Node serverNode)
     std::string version = serverNode.getProperty("minimumVersion",
                                                  std::string());
 
-    bool meetsMinimumVersion = compareStrI(version, PACKAGE_VERSION) <= 0;
+    bool meetsMinimumVersion = strcmp(version.c_str(), PACKAGE_VERSION) <= 0;
 
     // For display in the list
     if (meetsMinimumVersion)

src/main.cpp
@@ -25,6 +25,7 @@
 
 #include "utils/filesystem.h"
 #include "utils/gettext.h"
+#include "utils/stringutils.h"
 #include "utils/xml.h"
 
 #include <getopt.h>

src/net/manaserv/charhandler.cpp
@@ -44,6 +44,7 @@
 
 #include "utils/dtor.h"
 #include "utils/gettext.h"
+#include "utils/stringutils.h"
 
 extern Net::CharHandler *charHandler;
 extern ManaServ::GameHandler *gameHandler;

src/net/serverinfo.h
@@ -21,8 +21,6 @@
 
 #pragma once
 
-#include "utils/stringutils.h"
-
 #include <cstdint>
 #include <deque>
 #include <string>
@@ -74,12 +72,12 @@ public:
 
     static ServerType parseType(const std::string &type)
     {
-        if (compareStrI(type, "tmwathena") == 0)
+        if (type == "tmwathena")
             return ServerType::TMWATHENA;
         // Used for backward compatibility
-        if (compareStrI(type, "eathena") == 0)
+        if (type == "eathena")
             return ServerType::TMWATHENA;
-        if (compareStrI(type, "manaserv") == 0)
+        if (type == "manaserv")
             return ServerType::MANASERV;
         return ServerType::UNKNOWN;
     }

src/resources/theme.cpp
@@ -373,7 +373,7 @@ ResourceRef<Image> Theme::getImageFromTheme(const std::string &path)
 
 static int readColorType(const std::string &type)
 {
-    static std::string colors[] = {
+    static const char *colors[] = {
         "TEXT",
         "SHADOW",
         "OUTLINE",
@@ -419,12 +419,8 @@ static int readColorType(const std::string &type)
         return -1;
 
     for (int i = 0; i < Theme::THEME_COLORS_END; i++)
-    {
-        if (compareStrI(type, colors[i]) == 0)
-        {
+        if (type == colors[i])
             return i;
-        }
-    }
 
     return -1;
 }
@@ -463,7 +459,7 @@ static gcn::Color readColor(const std::string &description)
 
 static Palette::GradientType readColorGradient(const std::string &grad)
 {
-    static std::string grads[] = {
+    static const char *grads[] = {
         "STATIC",
         "PULSE",
         "SPECTRUM",
@@ -474,17 +470,15 @@ static Palette::GradientType readColorGradient(const std::string &grad)
         return Palette::STATIC;
 
     for (int i = 0; i < 4; i++)
-    {
-        if (compareStrI(grad, grads[i]))
-            return (Palette::GradientType) i;
-    }
+        if (grad == grads[i])
+            return static_cast<Palette::GradientType>(i);
 
     return Palette::STATIC;
 }
 
 static int readProgressType(const std::string &type)
 {
-    static std::string colors[] = {
+    static const char *colors[] = {
         "DEFAULT",
         "HP",
         "MP",
@@ -499,10 +493,8 @@ static int readProgressType(const std::string &type)
         return -1;
 
     for (int i = 0; i < Theme::THEME_PROG_END; i++)
-    {
-        if (compareStrI(type, colors[i]) == 0)
+        if (type == colors[i])
             return i;
-    }
 
     return -1;
 }

src/utils/stringutils.cpp
@@ -125,29 +125,6 @@ std::string removeColors(std::string msg)
     return msg;
 }
 
-int compareStrI(const std::string &a, const std::string &b)
-{
-    std::string::const_iterator itA = a.begin();
-    std::string::const_iterator endA = a.end();
-    std::string::const_iterator itB = b.begin();
-    std::string::const_iterator endB = b.end();
-
-    for (; itA < endA && itB < endB; ++itA, ++itB)
-    {
-        int comp = tolower(*itA) - tolower(*itB);
-        if (comp)
-            return comp;
-    }
-
-    // Check string lengths
-    if (itA == endA && itB == endB)
-        return 0;
-    else if (itA == endA)
-        return -1;
-    else
-        return 1;
-}
-
 bool isWordSeparator(char chr)
 {
     return (chr == ' ' || chr == ',' || chr == '.' || chr == '"');

src/utils/stringutils.h
@@ -115,16 +115,6 @@ std::string &replaceCharacters(std::string &str,
 std::string removeColors(std::string msg);
 
 /**
- * Compares the two strings case-insensitively.
- *
- * @param a the first string in the comparison
- * @param b the second string in the comparison
- * @return 0 if the strings are equal, positive if the first is greater,
- *           negative if the second is greater
- */
-int compareStrI(const std::string &a, const std::string &b);
-
-/**
  * Returns whether a string starts with a given prefix.
  */
 inline bool startsWith(const std::string &str, const char *prefix)

commit 3f630c76de06a0cbda163342b5ae0bf166aaad9f

Fixed loading of servers saved in client config
This was broken by 79e4325192f3260ed4ded264e43da8429650bf72 due to there
being a case difference between the serverTypeToString and parseType
functions, causing loaded servers to be invalid due to unknown server
type.

Closes #105

src/configuration.cpp
@@ -303,9 +303,9 @@ static const char *serverTypeToString(ServerType type)
     switch (type)
     {
     case ServerType::TmwAthena:
-        return "TmwAthena";
+        return "tmwathena";
     case ServerType::ManaServ:
-        return "ManaServ";
+        return "manaserv";
     default:
         return "";
     }
@@ -521,6 +521,7 @@ void deserialize(XML::Node node, ServerInfo &server)
     std::string type;
     node.attribute("type", type);
     server.type = ServerInfo::parseType(type);
+    server.save = true;
 
     for (auto node : node.children()) {
         if (node.name() == "connection") {

src/gui/serverdialog.cpp
@@ -511,13 +511,8 @@ void ServerDialog::loadServer(XML::Node serverNode)
 void ServerDialog::loadCustomServers()
 {
     for (auto &server : config.servers)
-    {
         if (server.isValid())
-        {
-            server.save = true;
             mServers.push_back(server);
-        }
-    }
 }
 
 void ServerDialog::saveCustomServers(const ServerInfo &currentServer, int index)